7bacbd361f517afb4d89571114f47f6186a3aa34
Yuhong Sun <yuhongsun96@gmail.com>
2023-05-05 14:59:04 -0700

    works now

--- backend/danswer/db/auth.py
+++ backend/danswer/db/auth.py
@@ -1,6 +1,8 @@
 from collections.abc import AsyncGenerator
 
 from danswer.db.engine import build_async_engine
+from danswer.db.models import AccessToken
+from danswer.db.models import User
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyUserDatabase
 from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
@@ -15,10 +17,10 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
-    yield SQLAlchemyUserDatabase(session, User)  # type: ignore
+    yield SQLAlchemyUserDatabase(session, User)
 
 
 async def get_access_token_db(
     session: AsyncSession = Depends(get_async_session),
 ):
-    yield SQLAlchemyAccessTokenDatabase(session, AccessToken)  # type: ignore
+    yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
