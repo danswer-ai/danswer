29803dce2b70e64e2ca78c286ff8fb8a6a0c1042
Yuhong Sun <yuhongsun96@gmail.com>
2023-05-05 17:29:44 -0700

    migration oops

---
+++ backend/alembic/versions/2666d766cb9b_google_oauth2.py
@@ -0,0 +1,55 @@
+"""Google OAuth2
+
+Revision ID: 2666d766cb9b
+Revises: 6d387b3196c2
+Create Date: 2023-05-05 15:49:35.716016
+
+"""
+import fastapi_users_db_sqlalchemy
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "2666d766cb9b"
+down_revision = "6d387b3196c2"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "oauth_account",
+        sa.Column("id", fastapi_users_db_sqlalchemy.generics.GUID(), nullable=False),
+        sa.Column(
+            "user_id",
+            fastapi_users_db_sqlalchemy.generics.GUID(),
+            nullable=False,
+        ),
+        sa.Column("oauth_name", sa.String(length=100), nullable=False),
+        sa.Column("access_token", sa.String(length=1024), nullable=False),
+        sa.Column("expires_at", sa.Integer(), nullable=True),
+        sa.Column("refresh_token", sa.String(length=1024), nullable=True),
+        sa.Column("account_id", sa.String(length=320), nullable=False),
+        sa.Column("account_email", sa.String(length=320), nullable=False),
+        sa.ForeignKeyConstraint(["user_id"], ["user.id"], ondelete="cascade"),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(
+        op.f("ix_oauth_account_account_id"),
+        "oauth_account",
+        ["account_id"],
+        unique=False,
+    )
+    op.create_index(
+        op.f("ix_oauth_account_oauth_name"),
+        "oauth_account",
+        ["oauth_name"],
+        unique=False,
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_oauth_account_oauth_name"), table_name="oauth_account")
+    op.drop_index(op.f("ix_oauth_account_account_id"), table_name="oauth_account")
+    op.drop_table("oauth_account")
