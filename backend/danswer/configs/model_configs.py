01080a1577b18f09e2a05077d14b442ff9d8d37c
Yuhong Sun <yuhongsun96@gmail.com>
2023-05-09 12:25:36 -0700

    headers

--- backend/danswer/configs/model_configs.py
+++ backend/danswer/configs/model_configs.py
@@ -1,8 +1,24 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# The NLP models used here are licensed under Apache 2.0
+# Specifically the sentence-transformers/all-distilroberta-v1 and cross-encoder/ms-marco-MiniLM-L-6-v2 models
+# The original creators can be found at https://www.sbert.net/index.html
 import os
 
 # Important considerations when choosing models
 # Max tokens count needs to be high considering use case (at least 512)
-# MIT or Apache license
+# Models used must be MIT or Apache license
 # Inference/Indexing speed
 
 # Bi/Cross-Encoder Model Configs
