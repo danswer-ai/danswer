312366eae1624165d9b8a63f5dedab39315705d5
Yuhong Sun <yuhongsun96@gmail.com>
2023-05-09 20:45:43 -0700

    DAN-45 Fix OAuth User Creation Flow (#22)

--- backend/danswer/db/auth.py
+++ backend/danswer/db/auth.py
@@ -1,15 +1,44 @@
+from typing import Any
+from typing import Dict
+
+from danswer.auth.schemas import UserRole
+from danswer.db.engine import build_async_engine
 from danswer.db.engine import get_async_session
 from danswer.db.models import AccessToken
 from danswer.db.models import OAuthAccount
 from danswer.db.models import User
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyUserDatabase
+from fastapi_users.models import UP
 from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
+from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+
+async def get_user_count() -> int:
+    async with AsyncSession(build_async_engine()) as asession:
+        stmt = select(func.count(User.id))
+        result = await asession.execute(stmt)
+        user_count = result.scalar()
+        if user_count is None:
+            raise RuntimeError("Was not able to fetch the user count.")
+        return user_count
+
+
+# Need to override this because FastAPI Users doesn't give flexibility for backend field creation logic in OAuth flow
+class SQLAlchemyUserAdminDB(SQLAlchemyUserDatabase):
+    async def create(self, create_dict: Dict[str, Any]) -> UP:
+        user_count = await get_user_count()
+        if user_count == 0:
+            create_dict["role"] = UserRole.ADMIN
+        else:
+            create_dict["role"] = UserRole.BASIC
+        return await super().create(create_dict)
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
-    yield SQLAlchemyUserDatabase(session, User, OAuthAccount)  # type: ignore
+    yield SQLAlchemyUserAdminDB(session, User, OAuthAccount)  # type: ignore
 
 
 async def get_access_token_db(
