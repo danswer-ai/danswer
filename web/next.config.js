2df3f8d9421fb3c676a795ff69ba27b7a4806099
Weves <chrisweaver101@gmail.com>
2023-05-30 14:57:43 -0700

    Fix streaming when running locally

--- web/next.config.js
+++ web/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   output: "standalone",
   rewrites: async () => {
     // In production, something else (nginx in the one box setup) should take
-    // care of this redirect. TODO (chris): better support setups where
+    // care of this rewrite. TODO (chris): better support setups where
     // web_server and api_server are on different machines.
     if (process.env.NODE_ENV === "production") return [];
 
@@ -14,6 +14,20 @@ const nextConfig = {
       {
         source: "/api/:path*",
         destination: "http://127.0.0.1:8080/:path*", // Proxy to Backend
+      },
+    ];
+  },
+  redirects: async () => {
+    // In production, something else (nginx in the one box setup) should take
+    // care of this redirect. TODO (chris): better support setups where
+    // web_server and api_server are on different machines.
+    if (process.env.NODE_ENV === "production") return [];
+
+    return [
+      {
+        source: "/api/stream-direct-qa:params*",
+        destination: "http://127.0.0.1:8080/stream-direct-qa:params*", // Proxy to Backend
+        permanent: true,
       },
     ];
   },
