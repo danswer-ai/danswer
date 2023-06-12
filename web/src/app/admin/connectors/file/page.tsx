7303959461dcd1e82f32583e2f2c1a0ca05c61b1
Weves <chrisweaver101@gmail.com>
2023-06-12 00:43:29 -0700

    Another small tweak

--- web/src/app/admin/connectors/file/page.tsx
+++ web/src/app/admin/connectors/file/page.tsx
@@ -95,7 +95,7 @@ export default function File() {
           </div>
         </div>{" "}
         where <i>{"<LINK>"}</i> is the link to the file. This will enable
-        Danswer to link users to the file when they click on the search result.
+        Danswer to link users to the original document when they click on the search result.
         More details on this can be found in the{" "}
         <a
           href="https://docs.danswer.dev/connectors/file"
