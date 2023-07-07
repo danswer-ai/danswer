284e8d41220d22e6d26aa05887d028f6b1dac53f
Weves <chrisweaver101@gmail.com>
2023-07-07 09:39:07 -0700

    Don't respond to messages in thread + add better formatting for slack messages

--- backend/danswer/listeners/slack_listener.py
+++ backend/danswer/listeners/slack_listener.py
@@ -3,6 +3,7 @@ import os
 from danswer.configs.app_configs import DANSWER_BOT_NUM_DOCS_TO_DISPLAY
 from danswer.configs.app_configs import DANSWER_BOT_NUM_RETRIES
 from danswer.configs.app_configs import QDRANT_DEFAULT_COLLECTION
+from danswer.configs.constants import DocumentSource
 from danswer.connectors.slack.utils import make_slack_api_rate_limited
 from danswer.direct_qa.answer_question import answer_question
 from danswer.server.models import QAResponse
@@ -34,6 +35,28 @@ def _get_socket_client() -> SocketModeClient:
     )
 
 
+_MAX_BLURB_LEN = 25
+
+
+def _build_custom_semantic_identifier(
+    semantic_identifier: str, blurb: str, source: str
+) -> str:
+    """
+    On slack, since we just show the semantic identifier rather than semantic + blurb, we need
+    to do some custom formatting to make sure the semantic identifier is unique and meaningful.
+    """
+    if source == DocumentSource.SLACK.value:
+        truncated_blurb = (
+            f"{blurb[:_MAX_BLURB_LEN]}..." if len(blurb) > _MAX_BLURB_LEN else blurb
+        )
+        if truncated_blurb:
+            return f"#{semantic_identifier}: {truncated_blurb}"
+        else:
+            return f"#{semantic_identifier}"
+
+    return semantic_identifier
+
+
 def _process_quotes(
     quotes: dict[str, dict[str, str | int | None]] | None
 ) -> tuple[str | None, list[str]]:
@@ -48,7 +71,12 @@ def _process_quotes(
         doc_name = quote_dict.get("semantic_identifier")
         if doc_link and doc_name and doc_id and doc_id not in doc_identifiers:
             doc_identifiers.append(str(doc_id))
-            quote_lines.append(f"- <{doc_link}|{doc_name}>")
+            custom_semantic_identifier = _build_custom_semantic_identifier(
+                semantic_identifier=doc_name,
+                blurb=quote_dict.get("blurb", ""),
+                source=quote_dict.get("source_type", ""),
+            )
+            quote_lines.append(f"- <{doc_link}|{custom_semantic_identifier}>")
 
     if not quote_lines:
         return None, []
@@ -65,16 +93,22 @@ def _process_documents(
         return None
 
     seen_docs_identifiers = set(already_displayed_doc_identifiers)
-    top_documents: list[SearchDoc] = []
+    top_document_lines: list[str] = []
     for d in documents:
         if d.document_id in seen_docs_identifiers:
             continue
         seen_docs_identifiers.add(d.document_id)
-        top_documents.append(d)
-        if len(top_documents) >= num_docs_to_display:
+
+        custom_semantic_identifier = _build_custom_semantic_identifier(
+            semantic_identifier=d.semantic_identifier,
+            blurb=d.blurb,
+            source=d.source_type,
+        )
+        top_document_lines.append(f"- <{d.link}|{custom_semantic_identifier}>")
+        if len(top_document_lines) >= num_docs_to_display:
             break
 
-    return "\n".join([f"- <{d.link}|{d.semantic_identifier}>" for d in top_documents])
+    return "\n".join(top_document_lines)
 
 
 def process_slack_event(client: SocketModeClient, req: SocketModeRequest) -> None:
@@ -100,8 +134,13 @@ def process_slack_event(client: SocketModeClient, req: SocketModeRequest) -> Non
             logger.info("Ignoring message from bot")
             return
 
+        message_ts = req.payload.get("event", {}).get("ts")
+        thread_ts = req.payload.get("event", {}).get("thread_ts")
+        if thread_ts and message_ts != thread_ts:
+            logger.info("Skipping message since it is not the root of a thread")
+            return
+
         msg = req.payload.get("event", {}).get("text")
-        thread_ts = req.payload.get("event", {}).get("ts")
         if not msg:
             logger.error("Unable to process empty message")
             return
@@ -168,7 +207,8 @@ def process_slack_event(client: SocketModeClient, req: SocketModeRequest) -> Non
             _respond_in_thread(
                 channel=req.payload.get("event", {}).get("channel"),
                 text=text,
-                thread_ts=thread_ts,
+                thread_ts=thread_ts
+                or message_ts,  # pick the root of the thread (if a thread exists)
             )
         except Exception:
             logger.exception(
