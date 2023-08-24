943661ca73c5c2c3cccf9905e7c387ae61fdbf57
Weves <chrisweaver101@gmail.com>
2023-08-24 10:53:58 -0700

    Add metadata read

--- backend/danswer/connectors/google_drive/constants.py
+++ backend/danswer/connectors/google_drive/constants.py
@@ -4,4 +4,7 @@ DB_CREDENTIALS_DICT_DELEGATED_USER_KEY = "google_drive_delegated_user"
 CRED_KEY = "credential_id_{}"
 GOOGLE_DRIVE_CRED_KEY = "google_drive_app_credential"
 GOOGLE_DRIVE_SERVICE_ACCOUNT_KEY = "google_drive_service_account_key"
-SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
+SCOPES = [
+    "https://www.googleapis.com/auth/drive.readonly",
+    "https://www.googleapis.com/auth/drive.metadata.readonly",
+]
