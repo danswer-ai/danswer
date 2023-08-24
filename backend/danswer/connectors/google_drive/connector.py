cebf91e75c1b511c9d1287573301f40f1afaafe4
Weves <chrisweaver101@gmail.com>
2023-08-23 20:46:15 -0700

    Add comment

--- backend/danswer/connectors/google_drive/connector.py
+++ backend/danswer/connectors/google_drive/connector.py
@@ -344,6 +344,12 @@ class GoogleDriveConnector(LoadConnector, PollConnector):
         return folder_ids
 
     def load_credentials(self, credentials: dict[str, Any]) -> dict[str, str] | None:
+        """Checks for two different types of credentials.
+        (1) A credential which holds a token acquired via a user going thorugh
+        the Google OAuth flow.
+        (2) A credential which holds a service account key JSON file, which
+        can then be used to impersonate any user in the workspace.
+        """
         creds = None
         new_creds_dict = None
         if DB_CREDENTIALS_DICT_TOKEN_KEY in credentials:
