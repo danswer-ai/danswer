2a613b9d5b92cdde0c2bebe9325f305fed8487d0
Yuhong Sun <yuhongsun96@gmail.com>
2023-08-27 12:50:13 -0700

    no migrations yet

--- backend/danswer/server/models.py
+++ backend/danswer/server/models.py
@@ -137,10 +137,7 @@ class QAFeedbackRequest(BaseModel):
 
 
 class SearchFeedbackRequest(BaseModel):
-<<<<<<< HEAD
     query_id: int
-=======
->>>>>>> 92fce76 (checkpoint)
     document_id: str
     document_rank: int
     click: bool
