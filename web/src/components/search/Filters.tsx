c033b8482a721cd4d61986677fc580bd7f7a9055
nlp8899 <142526738+nlp8899@users.noreply.github.com>
2023-10-02 13:13:23 -0400

    create a hubspot connector (#482)

--- web/src/components/search/Filters.tsx
+++ web/src/components/search/Filters.tsx
@@ -27,6 +27,7 @@ const sources: Source[] = [
   { displayName: "Notion", internalName: "notion" },
   { displayName: "Zulip", internalName: "zulip" },
   { displayName: "Linear", internalName: "linear" },
+  { displayName: "HubSpot", internalName: "hubspot" },
 ];
 
 interface SourceSelectorProps {
