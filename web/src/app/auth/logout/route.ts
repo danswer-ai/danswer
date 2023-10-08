a47f507ac247912ba254a9aa7a13dc0286d7e37c
Weves <chrisweaver101@gmail.com>
2023-10-07 22:59:43 -0700

    Fix logout typing

--- web/src/app/auth/logout/route.ts
+++ web/src/app/auth/logout/route.ts
@@ -5,5 +5,9 @@ export const POST = async (request: NextRequest) => {
   // Directs the logout request to the appropriate FastAPI endpoint.
   // Needed since env variables don't work well on the client-side
   const authType = await getAuthTypeSS();
-  return await logoutSS(authType, request.headers);
+  const response = await logoutSS(authType, request.headers);
+  if (response) {
+    return response;
+  }
+  return new Response(null, { status: 204 });
 };
