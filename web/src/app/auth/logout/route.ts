2ef31fd9bafccd50b9a33de385008fc5994496af
Chris Weaver <25087905+Weves@users.noreply.github.com>
2023-10-07 23:22:27 -0700

    Fix logout (#536)

--- web/src/app/auth/logout/route.ts
+++ web/src/app/auth/logout/route.ts
@@ -1,9 +1,14 @@
+import { getDomain } from "@/lib/redirectSS";
 import { getAuthTypeSS, logoutSS } from "@/lib/userSS";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (request: NextRequest) => {
   // Directs the logout request to the appropriate FastAPI endpoint.
   // Needed since env variables don't work well on the client-side
   const authType = await getAuthTypeSS();
-  return await logoutSS(authType, request.headers);
+  const response = await logoutSS(authType, request.headers);
+  if (response && response.ok) {
+    return NextResponse.redirect(new URL("/auth/login", getDomain(request)));
+  }
+  return new Response(null, { status: 204 });
 };
