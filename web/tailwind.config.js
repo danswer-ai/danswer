d3ffacae72eace4fdb5cff049fd95df9665568c8
Weves <chrisweaver101@gmail.com>
2023-10-21 13:02:17 -0700

    Adjust default Tremor color

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -74,7 +74,7 @@ module.exports = {
           },
           content: {
             subtle: "#4b5563", // gray-600
-            DEFAULT: "#6b7280", // gray-500
+            DEFAULT: "#9ca3af", // gray-400
             emphasis: "#e5e7eb", // gray-200
             strong: "#f9fafb", // gray-50
             inverted: "#000000", // black
