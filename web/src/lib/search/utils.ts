c679fc75aca53acb5f94b018560fc89df6ad7dbf
Yuhong Sun <yuhongsun96@gmail.com>
2023-10-21 21:04:23 -0700

    prettier

--- web/src/lib/search/utils.ts
+++ web/src/lib/search/utils.ts
@@ -2,7 +2,8 @@ import { Source } from "./interfaces";
 
 export const buildFilters = (sources: Source[], documentSets: string[]) => {
   const filters = {
-    source_type: sources.length > 0 ? sources.map((source) => source.internalName) : null,
+    source_type:
+      sources.length > 0 ? sources.map((source) => source.internalName) : null,
     document_set: documentSets.length > 0 ? documentSets : null,
     // TODO make this a date selector
     time_cutoff: null,
