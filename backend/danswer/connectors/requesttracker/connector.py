66deacb228f2ed5729a242300c27f07f5ca02471
Evan <esarmien@fas.harvard.edu>
2023-10-25 10:59:09 -0400

    Produces documents with sections for transactions. No attachments yet.

--- backend/danswer/connectors/requesttracker/connector.py
+++ backend/danswer/connectors/requesttracker/connector.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from datetime import timezone
+from logging import DEBUG as LOG_LVL_DEBUG
 from typing import Any
 from typing import List
 from typing import Optional
 from rt.rest1 import Rt
 from rt.rest1 import ALL_QUEUES
+from danswer.utils.logger import setup_logger
 from danswer.configs.app_configs import INDEX_BATCH_SIZE
 from danswer.configs.constants import DocumentSource
 from danswer.connectors.interfaces import GenerateDocumentsOutput
@@ -14,6 +16,8 @@ from danswer.connectors.interfaces import SecondsSinceUnixEpoch
 from danswer.connectors.models import ConnectorMissingCredentialError
 from danswer.connectors.models import Document
 from danswer.connectors.models import Section
+
+logger = setup_logger()
 
 
 class RequestTrackerConnector(LoadConnector, PollConnector):
@@ -30,6 +34,27 @@ class RequestTrackerConnector(LoadConnector, PollConnector):
         self.batch_size = batch_size
         self.rt_username = rt_username
         self.rt_password = rt_password
+
+    def txn_link(self, tid: int, txn: int):
+        return f"{self.base_url}/Ticket/Display.html?id={tid}&txn={txn}"
+
+    def build_doc_sections_from_txn(
+        self, connection: Rt, ticket_id: int
+    ) -> List[Section]:
+        Sections: List[Section] = []
+        for tx in connection.get_history(ticket_id):
+            Sections.append(
+                Section(
+                    link=self.txn_link(int(tx["id"]), ticket_id),
+                    text="\n".join(
+                        [
+                            f"{k}:\n{v}\n" if k != "Attachments" else ""
+                            for (k, v) in tx.items()
+                        ]
+                    ),
+                )
+            )
+        return Sections
 
     def load_credentials(self, credentials: dict[str, Any]) -> Optional[dict[str, Any]]:
         self.rt_username = credentials.get("requesttracker_username")
@@ -62,12 +87,13 @@ class RequestTrackerConnector(LoadConnector, PollConnector):
 
         for ticket in tickets:
             ticket_keys_to_omit = ["id", "Subject"]
-            id: int = int(ticket["numerical_id"])
-            ticketLink: str = f"{self.base_url}/Ticket/Display.html?id={id}"
-            ticketChildLink: str = f"{self.base_url}/Ticket/Display.html?id={id}&txn="
+            tid: int = int(ticket["numerical_id"])
+            ticketLink: str = f"{self.base_url}/Ticket/Display.html?id={tid}"
+            logger.info(f"Processing ticket {tid}")
             doc = Document(
                 id=ticket["id"],
-                sections=[Section(link=ticketLink, text=f"{ticket['Subject']}\n")],
+                sections=[Section(link=ticketLink, text=f"{ticket['Subject']}\n")]
+                + self.build_doc_sections_from_txn(Rt0, tid),
                 source=DocumentSource.REQUESTTRACKER,
                 semantic_identifier=ticket["Subject"],
                 metadata={
@@ -103,7 +129,7 @@ if __name__ == "__main__":
     from dotenv import load_dotenv
 
     load_dotenv()
-
+    logger.setLevel(LOG_LVL_DEBUG)
     rt_connector = RequestTrackerConnector("https://help.hmdc.harvard.edu")
     rt_connector.load_credentials(
         {
@@ -113,8 +139,8 @@ if __name__ == "__main__":
     )
 
     current = time.time()
-    one_day_ago = current - 24 * 60 * 60  # 1 days
+    one_day_ago = current - (24 * 60 * 60)  # 1 days
     latest_docs = rt_connector.poll_source(one_day_ago, current)
 
     for doc in latest_docs:
-        print("A")
+        print(doc)
