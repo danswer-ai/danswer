e6ea90927c763f980cad8b3e6f47a5513aa5fcfd
Yuhong Sun <yuhongsun96@gmail.com>
2023-11-02 17:41:31 -0700

    checkpoint

--- backend/danswer/direct_qa/qa_block.py
+++ backend/danswer/direct_qa/qa_block.py
@@ -1,38 +1,28 @@
 import abc
-import json
 import re
 from collections.abc import Callable
 from collections.abc import Iterator
 from copy import copy
 
 import tiktoken
-from langchain.schema.messages import AIMessage
 from langchain.schema.messages import BaseMessage
 from langchain.schema.messages import HumanMessage
-from langchain.schema.messages import SystemMessage
 
-from danswer.configs.constants import CODE_BLOCK_PAT
-from danswer.configs.constants import GENERAL_SEP_PAT
-from danswer.configs.constants import QUESTION_PAT
-from danswer.configs.constants import THOUGHT_PAT
-from danswer.configs.constants import UNCERTAINTY_PAT
 from danswer.direct_qa.interfaces import AnswerQuestionReturn
 from danswer.direct_qa.interfaces import AnswerQuestionStreamReturn
 from danswer.direct_qa.interfaces import DanswerAnswer
 from danswer.direct_qa.interfaces import DanswerQuotes
 from danswer.direct_qa.interfaces import QAModel
 from danswer.direct_qa.models import LLMMetricsContainer
-from danswer.direct_qa.qa_prompts import EMPTY_SAMPLE_JSON
-from danswer.direct_qa.qa_prompts import JsonChatProcessor
-from danswer.direct_qa.qa_prompts import WeakModelFreeformProcessor
 from danswer.direct_qa.qa_utils import process_answer
 from danswer.direct_qa.qa_utils import process_model_tokens
 from danswer.indexing.models import InferenceChunk
 from danswer.llm.interfaces import LLM
 from danswer.llm.utils import check_number_of_tokens
-from danswer.llm.utils import dict_based_prompt_to_langchain_prompt
 from danswer.llm.utils import get_default_llm_tokenizer
-from danswer.llm.utils import str_prompt_to_langchain_prompt
+from danswer.prompts.constants import CODE_BLOCK_PAT
+from danswer.prompts.direct_qa_prompts import COT_PROMPT
+from danswer.prompts.direct_qa_prompts import JSON_PROMPT
 from danswer.utils.logger import setup_logger
 from danswer.utils.text_processing import clean_up_code_blocks
 from danswer.utils.text_processing import escape_newlines
@@ -41,10 +31,31 @@ logger = setup_logger()
 
 
 class QAHandler(abc.ABC):
-    """Evolution of the `PromptProcessor` - handles both building the prompt and
-    processing the response. These are necessarily coupled, since the prompt determines
-    the response format (and thus how it should be parsed into an answer + quotes)."""
+    @abc.abstractmethod
+    def build_prompt(
+        self, query: str, context_chunks: list[InferenceChunk]
+    ) -> list[BaseMessage]:
+        raise NotImplementedError
 
+    @property
+    @abc.abstractmethod
+    def is_json_output(self) -> bool:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def process_llm_output(
+        self, model_output: str, context_chunks: list[InferenceChunk]
+    ) -> tuple[DanswerAnswer, DanswerQuotes]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def process_llm_token_stream(
+        self, tokens: Iterator[str], context_chunks: list[InferenceChunk]
+    ) -> AnswerQuestionStreamReturn:
+        raise NotImplementedError
+
+
+class JsonQAHandler(abc.ABC):
     @abc.abstractmethod
     def build_prompt(
         self, query: str, context_chunks: list[InferenceChunk]
@@ -53,7 +64,6 @@ class QAHandler(abc.ABC):
 
     @property
     def is_json_output(self) -> bool:
-        """Does the model expected to output a valid json"""
         return True
 
     def process_llm_output(
@@ -73,86 +83,42 @@ class QAHandler(abc.ABC):
         )
 
 
-class JsonChatQAHandler(QAHandler):
-    def build_prompt(
-        self, query: str, context_chunks: list[InferenceChunk]
-    ) -> list[BaseMessage]:
-        return dict_based_prompt_to_langchain_prompt(
-            JsonChatProcessor.fill_prompt(
-                question=query, chunks=context_chunks, include_metadata=False
-            )
-        )
-
-
-class SimpleChatQAHandler(QAHandler):
-    @property
-    def is_json_output(self) -> bool:
-        return False
-
-    def build_prompt(
-        self, query: str, context_chunks: list[InferenceChunk]
-    ) -> list[BaseMessage]:
-        return str_prompt_to_langchain_prompt(
-            WeakModelFreeformProcessor.fill_prompt(
-                question=query,
-                chunks=context_chunks,
-                include_metadata=False,
-            )
-        )
-
-
-class SingleMessageQAHandler(QAHandler):
+class SingleMessageQAHandler(JsonQAHandler):
     def build_prompt(
         self, query: str, context_chunks: list[InferenceChunk]
     ) -> list[BaseMessage]:
         context_docs_str = "\n".join(
-            f"{CODE_BLOCK_PAT.format(c.content)}" for c in context_chunks
+            f"\n{CODE_BLOCK_PAT.format(c.content)}\n" for c in context_chunks
         )
 
-        prompt: list[BaseMessage] = [
-            HumanMessage(
-                content="You are a question answering system that is constantly learning and improving. "
-                "You can process and comprehend vast amounts of text and utilize this knowledge "
-                "to provide accurate and detailed answers to diverse queries.\n"
-                "You ALWAYS responds with only a json containing an answer and quotes that support the answer.\n"
-                "Your responses are as INFORMATIVE and DETAILED as possible.\n"
-                f"{GENERAL_SEP_PAT}CONTEXT:\n\n{context_docs_str}"
-                f"{GENERAL_SEP_PAT}Sample response:"
-                f"{CODE_BLOCK_PAT.format(json.dumps(EMPTY_SAMPLE_JSON))}\n"
-                f"{QUESTION_PAT} {query}\n"
-                "Hint: Make the answer as DETAILED as possible and respond in JSON format!\n"
-                "Quotes MUST be EXACT substrings from provided documents!"
-            )
-        ]
+        single_message = JSON_PROMPT.format(
+            context_docs_str=context_docs_str, user_query=query
+        )
+
+        prompt: list[BaseMessage] = [HumanMessage(content=single_message)]
         return prompt
 
 
 class SingleMessageScratchpadHandler(QAHandler):
+    @property
+    def is_json_output(self) -> bool:
+        # Even though the full LLM output isn't a valid json
+        # only the valid json portion is kept and passed along
+        # therefore it should be treated as a json output
+        return True
+
     def build_prompt(
         self, query: str, context_chunks: list[InferenceChunk]
     ) -> list[BaseMessage]:
-        cot_block = (
-            f"{THOUGHT_PAT} Use this section as a scratchpad to reason through the answer.\n\n"
-            f"{json.dumps(EMPTY_SAMPLE_JSON)}"
-        )
-
         context_docs_str = "\n".join(
-            f"{CODE_BLOCK_PAT.format(c.content)}" for c in context_chunks
+            f"\n{CODE_BLOCK_PAT.format(c.content)}\n" for c in context_chunks
         )
 
-        prompt: list[BaseMessage] = [
-            HumanMessage(
-                content="You are a question answering system that is constantly learning and improving. "
-                "You can process and comprehend vast amounts of text and utilize this knowledge "
-                "to provide accurate and detailed answers to diverse queries.\n"
-                f"{GENERAL_SEP_PAT}CONTEXT:\n\n{context_docs_str}{GENERAL_SEP_PAT}"
-                f"You MUST respond in the following format:"
-                f"{CODE_BLOCK_PAT.format(cot_block)}\n"
-                f"{QUESTION_PAT} {query}\n"
-                "Hint: Make the answer as detailed as possible and use a JSON! "
-                "Quotes can ONLY be EXACT substrings from provided documents!"
-            )
-        ]
+        single_message = COT_PROMPT.format(
+            context_docs_str=context_docs_str, user_query=query
+        )
+
+        prompt: list[BaseMessage] = [HumanMessage(content=single_message)]
         return prompt
 
     def process_llm_output(
@@ -175,43 +141,29 @@ class SingleMessageScratchpadHandler(QAHandler):
     def process_llm_token_stream(
         self, tokens: Iterator[str], context_chunks: list[InferenceChunk]
     ) -> AnswerQuestionStreamReturn:
+        # Can be supported but the parsing is more involved, not handling until needed
         raise ValueError(
             "This Scratchpad approach is not suitable for real time uses like streaming"
         )
 
 
-class JsonChatQAUnshackledHandler(QAHandler):
+"""
+class SimpleChatQAHandler(QAHandler):
+    @property
+    def is_json_output(self) -> bool:
+        return False
+
     def build_prompt(
         self, query: str, context_chunks: list[InferenceChunk]
     ) -> list[BaseMessage]:
-        prompt: list[BaseMessage] = []
-
-        complete_answer_not_found_response = (
-            '{"answer": "' + UNCERTAINTY_PAT + '", "quotes": []}'
-        )
-        prompt.append(
-            SystemMessage(
-                content=(
-                    "Use the following pieces of context to answer the users question. Your response "
-                    "should be in JSON format and contain an answer and (optionally) quotes that help support the answer. "
-                    "Your responses should be informative, detailed, and consider all possibilities and edge cases. "
-                    f"If you don't know the answer, respond with '{complete_answer_not_found_response}'\n"
-                    f"Sample response:\n\n{json.dumps(EMPTY_SAMPLE_JSON)}"
-                )
+        return str_prompt_to_langchain_prompt(
+            WeakModelFreeformProcessor.fill_prompt(
+                question=query,
+                chunks=context_chunks,
+                include_metadata=False,
             )
         )
-        prompt.append(
-            SystemMessage(
-                content='Start by reading the following documents and responding with "Acknowledged".'
-            )
-        )
-        for chunk in context_chunks:
-            prompt.append(SystemMessage(content=chunk.content))
-            prompt.append(AIMessage(content="Acknowledged"))
-
-        prompt.append(HumanMessage(content=f"Question: {query}\n"))
-
-        return prompt
+"""
 
 
 def _tiktoken_trim_chunks(
@@ -243,9 +195,7 @@ class QABlock(QAModel):
         """This is called during server start up to load the models into memory
         in case the chosen LLM is not accessed via API"""
         if self._llm.requires_warm_up:
-            logger.info(
-                "Warming up LLM, this should only run for in memory LLMs like GPT4All"
-            )
+            logger.info("Warming up LLM with a first inference")
             self._llm.invoke("Ignore this!")
 
     def answer_question(
--- backend/danswer/prompts/direct_qa_prompts.py
+++ backend/danswer/prompts/direct_qa_prompts.py
@@ -1,7 +1,15 @@
+import json
+
+from danswer.prompts.constants import ANSWER_PAT
+from danswer.prompts.constants import GENERAL_SEP_PAT
+from danswer.prompts.constants import QUESTION_PAT
+from danswer.prompts.constants import QUOTE_PAT
+from danswer.prompts.constants import THOUGHT_PAT
 from danswer.prompts.constants import UNCERTAINTY_PAT
 
+
 QA_HEADER = """
-You are a question answering system that is constantly learning and improving. \
+You are a question answering system that is constantly learning and improving.
 You can process and comprehend vast amounts of text and utilize this knowledge to provide \
 accurate and detailed answers to diverse queries.
 """.strip()
@@ -10,6 +18,12 @@ accurate and detailed answers to diverse queries.
 REQUIRE_JSON = """
 You ALWAYS responds with only a json containing an answer and quotes that support the answer.
 Your responses are as INFORMATIVE and DETAILED as possible.
+""".strip()
+
+
+JSON_HELPFUL_HINT = """
+Hint: Make the answer as DETAILED as possible and respond in JSON format! \
+Quotes MUST be EXACT substrings from provided documents!
 """.strip()
 
 
@@ -25,6 +39,69 @@ EMPTY_SAMPLE_JSON = {
 ANSWER_NOT_FOUND_RESPONSE = f'{{"answer": "{UNCERTAINTY_PAT}", "quotes": []}}'
 
 
+# For weak LLM which only takes one chunk and cannot output json
+WEAK_LLM_PROMPT = f"""
+Reference Document:
+{{reference_doc}}
+{GENERAL_SEP_PAT}
+Answer the user query below based on the reference document above.
+Respond with an "{ANSWER_PAT}" section and as many "{QUOTE_PAT}" sections as needed to support the answer.'
+
+{QUESTION_PAT} {{question}}
+{ANSWER_PAT}
+""".strip()
+
+
+# For weak chat LLM which takes one chunk and cannot output json
+# The next message should have the user query
+WEAK_CHAT_LLM_PROMPT = f"""
+You are a question answering assistant
+Respond to the user query with an "{ANSWER_PAT}" section and \
+as many "{QUOTE_PAT}" sections as needed to support the answer.
+Answer the user query based on the following document:
+
+{{first_chunk_content}}
+""".strip()
+
+
+# Default json prompt which can use multiple docs
+JSON_PROMPT = f"""
+{QA_HEADER}
+{REQUIRE_JSON}
+{GENERAL_SEP_PAT}
+CONTEXT:
+{{context_docs_str}}
+{GENERAL_SEP_PAT}
+SAMPLE_RESPONSE:
+```
+{json.dumps(EMPTY_SAMPLE_JSON)}
+```
+{QUESTION_PAT} {{user_query}}
+{JSON_HELPFUL_HINT}
+""".strip()
+
+
+# Default chain-of-thought style json prompt which uses multiple docs
+# This one has a section for the LLM to output some non-answer "thoughts"
+# COT (chain-of-thought) flow basically
+COT_PROMPT = f"""
+{QA_HEADER}
+{GENERAL_SEP_PAT}
+CONTEXT:
+{{context_docs_str}}
+{GENERAL_SEP_PAT}
+You MUST respond in the following format:
+```
+{THOUGHT_PAT} Use this section as a scratchpad to reason through the answer.
+
+{json.dumps(EMPTY_SAMPLE_JSON)}
+```
+
+{QUESTION_PAT} {{user_query}}
+{JSON_HELPFUL_HINT}
+""".strip()
+
+
 # The following is provided for easy viewing of prompts
 if __name__ == "__main__":
-    print(ANSWER_NOT_FOUND_RESPONSE)
+    print(COT_PROMPT)
