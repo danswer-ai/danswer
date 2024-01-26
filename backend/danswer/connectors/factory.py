629e71a364e09084993fb7d029dbbef5c97f1312
Weves <chrisweaver101@gmail.com>
2024-01-25 17:26:14 -0800

    Fix import order

--- backend/danswer/connectors/factory.py
+++ backend/danswer/connectors/factory.py
@@ -25,13 +25,13 @@ from danswer.connectors.models import InputType
 from danswer.connectors.notion.connector import NotionConnector
 from danswer.connectors.productboard.connector import ProductboardConnector
 from danswer.connectors.requesttracker.connector import RequestTrackerConnector
+from danswer.connectors.sharepoint.connector import SharepointConnector
 from danswer.connectors.slab.connector import SlabConnector
 from danswer.connectors.slack.connector import SlackLoadConnector
 from danswer.connectors.slack.connector import SlackPollConnector
 from danswer.connectors.web.connector import WebConnector
 from danswer.connectors.zendesk.connector import ZendeskConnector
 from danswer.connectors.zulip.connector import ZulipConnector
-from danswer.connectors.sharepoint.connector import SharepointConnector
 
 
 class ConnectorMissingException(Exception):
