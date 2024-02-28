371b3ab5d2c881b5af079c353cbf538be3fc0ccf
Yuhong Sun <yuhongsun96@gmail.com>
2024-02-27 23:43:07 -0800

    touchups

--- backend/danswer/search/search_nlp_models.py
+++ backend/danswer/search/search_nlp_models.py
@@ -1,5 +1,4 @@
 import gc
-import logging
 import os
 from enum import Enum
 from typing import Optional
@@ -7,6 +6,7 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 import requests
+from transformers import logging as transformer_logging  # type:ignore
 
 from danswer.configs.app_configs import MODEL_SERVER_HOST
 from danswer.configs.app_configs import MODEL_SERVER_PORT
@@ -26,10 +26,10 @@ from shared_models.model_server_models import RerankResponse
 
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["HF_HUB_DISABLE_TELEMETRY"] = "1"
 
 logger = setup_logger()
-# Remove useless info about layer initialization
-logging.getLogger("transformers").setLevel(logging.ERROR)
+transformer_logging.set_verbosity_error()
 
 
 if TYPE_CHECKING:
