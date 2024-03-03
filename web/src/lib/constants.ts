563df1f952c28c8fb3547a5179c052a61bfaf814
Weves <chrisweaver101@gmail.com>
2024-03-03 15:27:20 -0800

    Add env variable to allow people to control what clicking on New Chat does

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -4,6 +4,10 @@ export const INTERNAL_URL = process.env.INTERNAL_URL || "http://127.0.0.1:8080";
 export const NEXT_PUBLIC_DISABLE_STREAMING =
   process.env.NEXT_PUBLIC_DISABLE_STREAMING?.toLowerCase() === "true";
 
+export const NEXT_PUBLIC_NEW_CHAT_DIRECTS_TO_SAME_PERSONA =
+  process.env.NEXT_PUBLIC_NEW_CHAT_DIRECTS_TO_SAME_PERSONA?.toLowerCase() ===
+  "true";
+
 export const GMAIL_AUTH_IS_ADMIN_COOKIE_NAME = "gmail_auth_is_admin";
 
 export const GOOGLE_DRIVE_AUTH_IS_ADMIN_COOKIE_NAME =
