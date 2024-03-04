beb7fa4338ce8fa0c1c4e42b43e70953f9045f48
Weves <chrisweaver101@gmail.com>
2024-03-04 00:47:12 -0800

    Add rate limit detection for confluence

--- backend/danswer/connectors/confluence/rate_limit_handler.py
+++ backend/danswer/connectors/confluence/rate_limit_handler.py
@@ -1,46 +1,34 @@
-import os
 from collections.abc import Callable
 from typing import Any
+from typing import cast
 from typing import TypeVar
 
 from requests import HTTPError
+from retry import retry
 
 
 F = TypeVar("F", bound=Callable[..., Any])
 
 
-def wrap_confluence_call(confluence_call: F) -> F:
+class ConfluenceRateLimitError(Exception):
+    pass
+
+
+def make_confluence_call_handle_rate_limit(confluence_call: F) -> F:
+    @retry(
+        exceptions=ConfluenceRateLimitError,
+        tries=10,
+        delay=1,
+        max_delay=600,  # 10 minutes
+        backoff=2,
+        jitter=1,
+    )
     def wrapped_call(*args: list[Any], **kwargs: Any) -> Any:
         try:
-            return confluence_call()
+            return confluence_call(*args, **kwargs)
         except HTTPError as e:
-            print(e)
+            if e.response.status_code == 429:
+                raise ConfluenceRateLimitError()
+            raise
 
-
-if __name__ == "__main__":
-    import threading
-    from danswer.connectors.confluence.connector import ConfluenceConnector
-
-    connector = ConfluenceConnector(os.environ["CONFLUENCE_TEST_SPACE_URL"])
-    connector.load_credentials(
-        {
-            "confluence_username": os.environ["CONFLUENCE_USER_NAME"],
-            "confluence_access_token": os.environ["CONFLUENCE_ACCESS_TOKEN"],
-        }
-    )
-
-    while True:
-        threads = [
-            threading.Thread(
-                target=lambda: connector._fetch_comments(
-                    connector.confluence_client, 1146902
-                )
-            )
-            for _ in range(500)
-        ]
-        for t in threads:
-            t.start()
-
-        for t in threads:
-            t.join()
-        print("done")
+    return cast(F, wrapped_call)
