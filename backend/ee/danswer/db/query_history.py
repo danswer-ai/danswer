e3ef620094fee3f1d497e27f69b7b4fd7ee88429
Yuhong Sun <yuhongsun96@gmail.com>
2024-03-27 21:06:46 -0700

    Query History Now Handles Old Messages (#58)

--- backend/ee/danswer/db/query_history.py
+++ backend/ee/danswer/db/query_history.py
@@ -1,6 +1,8 @@
 import datetime
 from typing import Literal
 
+from sqlalchemy import asc
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
 from danswer.db.models import ChatSession
@@ -9,12 +11,23 @@ SortByOptions = Literal["time_sent"]
 
 
 def fetch_chat_sessions_by_time(
-    start: datetime.datetime, end: datetime.datetime, db_session: Session
+    start: datetime.datetime,
+    end: datetime.datetime,
+    db_session: Session,
+    ascending: bool = False,
+    limit: int | None = 500,
 ) -> list[ChatSession]:
-    chat_sessions = (
+    order = asc(ChatSession.time_created) if ascending else desc(ChatSession.time_created)  # type: ignore
+
+    query = (
         db_session.query(ChatSession)
         .filter(ChatSession.time_created >= start, ChatSession.time_created <= end)
-        .all()
+        .order_by(order)
     )
+
+    if limit is not None:
+        query = query.limit(limit)
+
+    chat_sessions = query.all()
 
     return chat_sessions
