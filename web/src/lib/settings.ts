8a29d0ac6692490973fca9af5e2747baa2e2a1ca
Weves <chrisweaver101@gmail.com>
2024-03-31 23:49:49 -0700

    Fix fetch settings SS

--- web/src/lib/settings.ts
+++ web/src/lib/settings.ts
@@ -1,8 +1,8 @@
 import { Settings } from "@/app/admin/settings/interfaces";
-import { buildUrl } from "./utilsSS";
+import { fetchSS } from "./utilsSS";
 
 export async function getSettingsSS(): Promise<Settings | null> {
-  const response = await fetch(buildUrl("/settings"));
+  const response = await fetchSS("/settings");
   if (response.ok) {
     return await response.json();
   }
