5b1ce750b8f24d254d84893a70560e9115302970
Alex Co <tuanclq@gmail.com>
2024-04-19 20:20:47 +0800

    Add API health check (#8)
    * Add API health check
    
    Signed-off-by: Alex Co <alex.tuan@mindvalley.com>
    
    * Change library
    
    Signed-off-by: Alex Co <alex.tuan@mindvalley.com>
    
    * reformatted
    
    Signed-off-by: Alex Co <alex.tuan@mindvalley.com>
    
    ---------
    
    Signed-off-by: Alex Co <alex.tuan@mindvalley.com>

--- backend/danswer/main.py
+++ backend/danswer/main.py
@@ -7,10 +7,12 @@ from typing import cast
 import uvicorn
 from fastapi import APIRouter
 from fastapi import FastAPI
+ 
 from fastapi import Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi_health import health
 from httpx_oauth.clients.google import GoogleOAuth2
 from sqlalchemy.orm import Session
 
@@ -342,6 +344,11 @@ def get_application() -> FastAPI:
     application.add_exception_handler(
         RequestValidationError, validation_exception_handler
     )
+
+    def healthz_check():
+        return True
+ 
+    application.add_api_route("/healthz", health([healthz_check]))
 
     application.add_exception_handler(ValueError, value_error_handler)
 
