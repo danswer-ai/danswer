3399d54619a5943d04522b3e918b9ebc1df2613d
ptitzlabs <a.e.naruta@gmail.com>
2024-04-28 19:32:21 +0200

    Create tokens.py

---
+++ backend/danswer/danswerbot/telegram/tokens.py
@@ -0,0 +1,21 @@
+import os
+
+from pydantic import BaseModel
+from pydantic import root_validator
+from pydantic import validator
+
+from typing import cast
+
+from danswer.dynamic_configs.factory import get_dynamic_config_store
+
+class TelegramBotToken(BaseModel):
+  bot_token: str
+    
+  class Config:
+    frozen = True
+
+_TELEGRAM_BOT_TOKEN_CONFIG_KEY = "telegram_bot_token_config_key"
+
+def fetch_token():
+  bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+  return TelegramBotToken(bot_token=bot_token)
