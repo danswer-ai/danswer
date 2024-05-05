10be91a8cca91b3454f802f215dde06800543c2f
Yuhong Sun <yuhongsun96@gmail.com>
2024-05-05 16:02:42 -0700

    Track Slack questions Autoresolved (#86)

--- backend/ee/danswer/db/analytics.py
+++ backend/ee/danswer/db/analytics.py
@@ -6,6 +6,7 @@ from sqlalchemy import case
 from sqlalchemy import cast
 from sqlalchemy import Date
 from sqlalchemy import func
+from sqlalchemy import or_
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -16,9 +17,9 @@ from danswer.db.models import ChatSession
 
 
 def fetch_query_analytics(
-    db_session: Session,
     start: datetime.datetime,
     end: datetime.datetime,
+    db_session: Session,
 ) -> Sequence[tuple[int, int, int, datetime.date]]:
     stmt = (
         select(
@@ -51,9 +52,9 @@ def fetch_query_analytics(
 
 
 def fetch_per_user_query_analytics(
-    db_session: Session,
     start: datetime.datetime,
     end: datetime.datetime,
+    db_session: Session,
 ) -> Sequence[tuple[int, int, int, datetime.date, UUID]]:
     stmt = (
         select(
@@ -80,3 +81,92 @@ def fetch_per_user_query_analytics(
     )
 
     return db_session.execute(stmt).all()  # type: ignore
+
+
+def fetch_danswerbot_analytics(
+    start: datetime.datetime,
+    end: datetime.datetime,
+    db_session: Session,
+) -> Sequence[tuple[int, int, datetime.date]]:
+    """Gets the:
+    Date of each set of aggregated statistics
+    Number of DanswerBot Queries (Chat Sessions)
+    Number of instances of Negative feedback OR Needing additional help
+        (only counting the last feedback)
+    """
+    # Get every chat session in the time range which is a Danswerbot flow
+    # along with the first Assistant message which is the response to the user question.
+    # Generally there should not be more than one AI message per chat session of this type
+    subquery_first_ai_response = (
+        db_session.query(
+            ChatMessage.chat_session_id.label("chat_session_id"),
+            func.min(ChatMessage.id).label("chat_message_id"),
+        )
+        .join(ChatSession, ChatSession.id == ChatMessage.chat_session_id)
+        .where(
+            ChatSession.time_created >= start,
+            ChatSession.time_created <= end,
+            ChatSession.danswerbot_flow.is_(True),
+        )
+        .where(
+            ChatMessage.message_type == MessageType.ASSISTANT,
+        )
+        .group_by(ChatMessage.chat_session_id)
+        .subquery()
+    )
+
+    # Get the chat message ids and most recent feedback for each of those chat messages,
+    # not including the messages that have no feedback
+    subquery_last_feedback = (
+        db_session.query(
+            ChatMessageFeedback.chat_message_id.label("chat_message_id"),
+            func.max(ChatMessageFeedback.id).label("max_feedback_id"),
+        )
+        .group_by(ChatMessageFeedback.chat_message_id)
+        .subquery()
+    )
+
+    results = (
+        db_session.query(
+            func.count(ChatSession.id).label("total_sessions"),
+            # Need to explicitly specify this as False to handle the NULL case so the cases without
+            # feedback aren't counted against Danswerbot
+            func.sum(
+                case(
+                    (
+                        or_(
+                            ChatMessageFeedback.is_positive.is_(False),
+                            ChatMessageFeedback.required_followup,
+                        ),
+                        1,
+                    ),
+                    else_=0,
+                )
+            ).label("negative_answer"),
+            cast(ChatSession.time_created, Date).label("session_date"),
+        )
+        .join(
+            subquery_first_ai_response,
+            ChatSession.id == subquery_first_ai_response.c.chat_session_id,
+        )
+        # Combine the chat sessions with latest feedback to get the latest feedback for the first AI
+        # message of the chat session where the chat session is Danswerbot type and within the time
+        # range specified. Left/outer join used here to ensure that if no feedback, a null is used
+        # for the feedback id
+        .outerjoin(
+            subquery_last_feedback,
+            subquery_first_ai_response.c.chat_message_id
+            == subquery_last_feedback.c.chat_message_id,
+        )
+        # Join the actual feedback table to get the feedback info for the sums
+        # Outer join because the "last feedback" may be null
+        .outerjoin(
+            ChatMessageFeedback,
+            ChatMessageFeedback.id == subquery_last_feedback.c.max_feedback_id,
+        )
+        .group_by(cast(ChatSession.time_created, Date))
+        .order_by(cast(ChatSession.time_created, Date))
+        .all()
+    )
+
+    return results
