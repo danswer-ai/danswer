5d1bd3eb437923e8b4d6a902429efbb0a493b83a
ptitzlabs <a.e.naruta@gmail.com>
2024-05-08 22:33:53 +0200

    Updated local launcher

--- web/local_launch.sh
+++ web/local_launch.sh
@@ -5,7 +5,7 @@ LOCAL_PORT=8080
 REMOTE_PORT=8080
 SSH_USER=ubuntu
 SSH_HOST=mir.svoi.fr
-WEB_DOMAIN=localhost
+export WEB_DOMAIN=http://localhost:3000
 
 # Check if tunnel is up by checking if anything is listening on the LOCAL_PORT
 if lsof -i :$LOCAL_PORT | grep LISTEN > /dev/null
@@ -13,7 +13,7 @@ then
     echo "SSH tunnel already established."
 else
     echo "Setting up SSH tunnel..."
-    ssh -N -L ${LOCAL_PORT}:localhost:${REMOTE_PORT} ${SSH_USER}@${SSH_HOST} &
+    ssh -N -L ${LOCAL_PORT}:localhost:${REMOTE_PORT} ${SSH_USER}@${SSH_HOST} -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" &
     echo "SSH tunnel established on port ${LOCAL_PORT}."
 fi
 
