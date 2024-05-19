e21a3793c11d3bb69264249124726abf7c3fdf30
Yuhong Sun <yuhongsun96@gmail.com>
2024-05-19 11:39:43 -0700

    checkpoint

--- backend/danswer/file_store/utils.py
+++ backend/danswer/file_store/utils.py
@@ -1,6 +1,5 @@
 from io import BytesIO
 from typing import cast
-from uuid import UUID
 from uuid import uuid4
 
 import requests
@@ -10,23 +9,13 @@ from danswer.configs.constants import FileOrigin
 from danswer.db.engine import get_session_context_manager
 from danswer.db.models import ChatMessage
 from danswer.file_store.file_store import get_default_file_store
-from danswer.file_store.models import ChatFileType
 from danswer.file_store.models import InMemoryChatFile
 from danswer.utils.threadpool_concurrency import run_functions_tuples_in_parallel
 
 
-def build_chat_file_name(
-    file_name: str,
-    file_type: ChatFileType,
-    unique_id: UUID
-    | str,  # Needed to allow multiple copies of a file to exist with the same name
-) -> str:
-    return f"chat__{file_type.value}__{file_name}__{unique_id}"
-
-
-def load_chat_file(full_file_name: str, db_session: Session) -> InMemoryChatFile:
-    file_io = get_default_file_store(db_session).read_file(full_file_name, mode="b")
-    return InMemoryChatFile(file_id=full_file_name, content=file_io.read())
+def load_chat_file(file_id: str, db_session: Session) -> InMemoryChatFile:
+    file_io = get_default_file_store(db_session).read_file(file_id, mode="b")
+    return InMemoryChatFile(file_id=file_id, content=file_io.read())
 
 
 def load_all_chat_files(
