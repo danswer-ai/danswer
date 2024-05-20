79d133c4acca987cbc8a6e8c3d7b4dd447d0bdd8
Alex Co <tuanclq@gmail.com>
2024-05-20 15:33:31 +0800

    Apply re-branding modifications (#29)
    Signed-off-by: Alex Co <alex.tuan@mindvalley.com>

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "Danswer",
+  title: "Eve AI",
   description: "Question answering for your documents",
 };
 
--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -73,7 +73,7 @@ export function Header({ user }: HeaderProps) {
               <Image src="/logo.png" alt="Logo" width="1419" height="1520" />
             </div>
             <h1 className="flex text-2xl text-strong font-bold my-auto">
-              Danswer
+              Eve<sup className="ai-superscript">AI</sup>
             </h1>
           </div>
         </Link>
