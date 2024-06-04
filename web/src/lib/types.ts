55a7537f53c725d8fde594a3faa7f6e18a53ca70
“Pablo <pablo@danswer.ai>
2024-06-03 17:03:31 -0700

    formatting

--- web/src/lib/types.ts
+++ web/src/lib/types.ts
@@ -213,7 +213,7 @@ export interface IndexAttemptSnapshot {
 
 export interface ConnectorIndexingStatus<
   ConnectorConfigType,
-  ConnectorCredentialType
+  ConnectorCredentialType,
 > {
   cc_pair_id: number;
   name: string | null;
