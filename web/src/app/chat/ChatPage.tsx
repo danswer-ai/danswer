714e71f3edbfbc4913770fc7b25efe1e608cb748
“Pablo <pablo@danswer.ai>
2024-06-04 10:48:53 -0700

    add gap-based scroll hook

--- web/src/app/chat/ChatPage.tsx
+++ web/src/app/chat/ChatPage.tsx
@@ -41,7 +41,10 @@ import { useContext, useEffect, useRef, useState } from "react";
 import { usePopup } from "@/components/admin/connectors/Popup";
 import { SEARCH_PARAM_NAMES, shouldSubmitOnLoad } from "./searchParams";
 import { useDocumentSelection } from "./useDocumentSelection";
-import { useFilters, useLlmOverride } from "@/lib/hooks";
+import useAutoScrollOnMessage, {
+  useFilters,
+  useLlmOverride,
+} from "@/lib/hooks";
 import { computeAvailableFilters } from "@/lib/filters";
 import { FeedbackType } from "./types";
 import ResizableSection from "@/components/resizable/ResizableSection";
@@ -411,30 +414,8 @@ export function ChatPage({
   const lastMessageRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLDivElement>(null);
   const endDivRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    // new auto scroll GPT Style
-    if (
-      isStreaming &&
-      lastMessageRef &&
-      inputRef &&
-      inputRef.current &&
-      lastMessageRef.current
-    ) {
-      const lastMessageRect = lastMessageRef.current.getBoundingClientRect();
-      const endDivRect = inputRef.current.getBoundingClientRect();
 
-      if (endDivRect.bottom - lastMessageRect.bottom > 140) {
-        if (endDivRef && endDivRef?.current) {
-          endDivRef?.current.scrollIntoView({ behavior: "smooth" });
-        }
-      }
-    }
-
-    // old version
-    // if (isStreaming || !message {
-    //   handleAutoScroll(endDivRef, scrollableDivRef);
-    // }
-  });
+  useAutoScrollOnMessage({ isStreaming, lastMessageRef, inputRef, endDivRef });
 
   // scroll to bottom initially
   const [hasPerformedInitialScroll, setHasPerformedInitialScroll] =
@@ -445,105 +426,105 @@ export function ChatPage({
   }, [isFetchingChatMessages]);
 
   // TODO: new scrolling feature
-  useEffect(() => {
-    let timeoutId: NodeJS.Timeout | null = null;
-    let prevInputHeight = 0;
-    let prevDistance = 0;
+  // useEffect(() => {
+  //   let timeoutId: NodeJS.Timeout | null = null;
+  //   let prevInputHeight = 0;
+  //   let prevDistance = 0;
 
-    const handleInputResize = async () => {
-      function delay(ms: number) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-      }
+  //   const handleInputResize = async () => {
+  //     function delay(ms: number) {
+  //       return new Promise((resolve) => setTimeout(resolve, ms));
+  //     }
 
-      await delay(100);
+  //     await delay(100);
 
-      if (
-        lastMessageRef &&
-        inputRef &&
-        inputRef.current &&
-        lastMessageRef.current
-      ) {
-        const lastMessageRect = lastMessageRef.current.getBoundingClientRect();
-        const endDivRect = inputRef.current.getBoundingClientRect();
-        const currentInputHeight = endDivRect.height;
-        console.log(
-          `current vs previous heights ${currentInputHeight} | ${prevInputHeight}`
-        );
-        console.log(
-          `distance compared to previous ${endDivRect.top - lastMessageRect.bottom - (currentInputHeight - prevInputHeight)}`
-        );
+  //     if (
+  //       lastMessageRef &&
+  //       inputRef &&
+  //       inputRef.current &&
+  //       lastMessageRef.current
+  //     ) {
+  //       const lastMessageRect = lastMessageRef.current.getBoundingClientRect();
+  //       const endDivRect = inputRef.current.getBoundingClientRect();
+  //       const currentInputHeight = endDivRect.height;
+  //       console.log(
+  //         `current vs previous heights ${currentInputHeight} | ${prevInputHeight}`
+  //       );
+  //       console.log(
+  //         `distance compared to previous ${endDivRect.top - lastMessageRect.bottom - (currentInputHeight - prevInputHeight)}`
+  //       );
 
-        if (
-          currentInputHeight !== prevInputHeight &&
-          currentInputHeight > prevInputHeight &&
-          endDivRect.top <= lastMessageRect.bottom
-        ) {
-          if (
-            lastMessageRef &&
-            inputRef &&
-            inputRef.current &&
-            lastMessageRef.current
-          ) {
-            const lastMessageRect =
-              lastMessageRef.current.getBoundingClientRect();
-            const endDivRect = inputRef.current.getBoundingClientRect();
+  //       if (
+  //         currentInputHeight !== prevInputHeight &&
+  //         currentInputHeight > prevInputHeight &&
+  //         endDivRect.top <= lastMessageRect.bottom
+  //       ) {
+  //         if (
+  //           lastMessageRef &&
+  //           inputRef &&
+  //           inputRef.current &&
+  //           lastMessageRef.current
+  //         ) {
+  //           const lastMessageRect =
+  //             lastMessageRef.current.getBoundingClientRect();
+  //           const endDivRect = inputRef.current.getBoundingClientRect();
 
-            if (prevDistance > -100) {
-              if (endDivRef && endDivRef?.current) {
-                console.log("Exists");
-                if (timeoutId) {
-                  clearTimeout(timeoutId);
-                }
+  //           if (prevDistance > -100) {
+  //             if (endDivRef && endDivRef?.current) {
+  //               console.log("Exists");
+  //               if (timeoutId) {
+  //                 clearTimeout(timeoutId);
+  //               }
 
-                timeoutId = setTimeout(() => {
-                  if (endDivRef && endDivRef?.current) {
-                    // TODO
-                    // window.scrollBy({
-                    //   top: currentInputHeight - prevInputHeight,
-                    //   behavior: 'smooth',
-                    // });
+  //               timeoutId = setTimeout(() => {
+  //                 if (endDivRef && endDivRef?.current) {
+  //                   // TODO
+  //                   // window.scrollBy({
+  //                   //   top: currentInputHeight - prevInputHeight,
+  //                   //   behavior: 'smooth',
+  //                   // });
 
-                    endDivRef?.current.scrollIntoView({ behavior: "smooth" });
-                  }
-                }, 500);
-              }
-            }
-          }
+  //                   endDivRef?.current.scrollIntoView({ behavior: "smooth" });
+  //                 }
+  //               }, 500);
+  //             }
+  //           }
+  //         }
 
-          prevInputHeight = currentInputHeight;
-        }
-        if (currentInputHeight !== prevInputHeight) {
-          prevInputHeight = currentInputHeight;
-        }
-      }
+  //         prevInputHeight = currentInputHeight;
+  //       }
+  //       if (currentInputHeight !== prevInputHeight) {
+  //         prevInputHeight = currentInputHeight;
+  //       }
+  //     }
 
-      if (
-        lastMessageRef &&
-        inputRef &&
-        inputRef.current &&
-        lastMessageRef.current
-      ) {
-        const lastMessageRect = lastMessageRef.current.getBoundingClientRect();
-        const endDivRect = inputRef.current.getBoundingClientRect();
-        prevDistance = endDivRect.top - lastMessageRect.bottom;
-      }
-    };
+  //     if (
+  //       lastMessageRef &&
+  //       inputRef &&
+  //       inputRef.current &&
+  //       lastMessageRef.current
+  //     ) {
+  //       const lastMessageRect = lastMessageRef.current.getBoundingClientRect();
+  //       const endDivRect = inputRef.current.getBoundingClientRect();
+  //       prevDistance = endDivRect.top - lastMessageRect.bottom;
+  //     }
+  //   };
 
-    // Previous- on input
-    const textarea = textAreaRef.current;
-    if (textarea) {
-      textarea.addEventListener("input", handleInputResize);
-    }
+  //   // Previous- on input
+  //   const textarea = textAreaRef.current;
+  //   if (textarea) {
+  //     textarea.addEventListener("input", handleInputResize);
+  //   }
 
-    return () => {
-      if (textarea) {
-        textarea.removeEventListener("input", handleInputResize);
-      }
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
-    };
-  }, [lastMessageRef, inputRef]);
+  //   return () => {
+  //     if (textarea) {
+  //       textarea.removeEventListener("input", handleInputResize);
+  //     }
+  //     if (timeoutId) {
+  //       clearTimeout(timeoutId);
+  //     }
+  //   };
+  // }, [lastMessageRef, inputRef]);
 
   // handle re-sizing of the text area
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
