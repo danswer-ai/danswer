750c1df0bbdddeedfb036f085ead7a4a850b6ef1
Hagen O'Neill <hagen@danswer.ai>
2024-06-04 13:23:02 -0700

    ran prettier

--- web/src/lib/types.ts
+++ web/src/lib/types.ts
@@ -218,7 +218,7 @@ export interface IndexAttemptSnapshot {
 
 export interface ConnectorIndexingStatus<
   ConnectorConfigType,
-  ConnectorCredentialType,
+  ConnectorCredentialType
 > {
   cc_pair_id: number;
   name: string | null;
