f691462252f5b573c15e3969336915e753396d78
pablodanswer <pablo@danswer.ai>
2024-06-06 16:58:02 -0700

    functional assistants api

--- backend/danswer/chat/process_message.py
+++ backend/danswer/chat/process_message.py
@@ -228,8 +228,6 @@ def stream_chat_message_objects(
             )
         else:
             persona = chat_session.persona
-        print("persona")
-        print(persona)
 
         prompt_id = new_msg_req.prompt_id
         if prompt_id is None and persona.prompts:
@@ -246,6 +244,7 @@ def stream_chat_message_objects(
                 llm_override=new_msg_req.llm_override or chat_session.llm_override,
                 additional_headers=litellm_additional_headers,
             )
+
         except GenAIDisabledException:
             raise RuntimeError("LLM is disabled. Can't use chat flow without LLM.")
 
@@ -270,6 +269,7 @@ def stream_chat_message_objects(
                 user_id=user_id,
                 db_session=db_session,
             )
+
         else:
             parent_message = root_message
 
@@ -396,11 +396,15 @@ def stream_chat_message_objects(
         if not final_msg.prompt:
             raise RuntimeError("No Prompt found")
 
-        prompt_config = PromptConfig.from_model(
-            final_msg.prompt,
-            prompt_override=(
-                new_msg_req.prompt_override or chat_session.prompt_override
-            ),
+        prompt_config = (
+            PromptConfig.from_model(
+                final_msg.prompt,
+                prompt_override=(
+                    new_msg_req.prompt_override or chat_session.prompt_override
+                ),
+            )
+            if not persona
+            else PromptConfig.from_model(persona.prompts[0])
         )
 
         persona_tool_classes = [
@@ -458,7 +462,6 @@ def stream_chat_message_objects(
                     dalle_key = openai_provider.api_key
                 tools.append(ImageGenerationTool(api_key=dalle_key))
 
-        # LLM prompt building, response capturing, etc.
         answer = Answer(
             question=final_msg.message,
             latest_query_files=latest_query_files,
