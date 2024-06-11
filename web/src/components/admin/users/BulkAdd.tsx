304a8e926a42738cf7896892f56089466a0909bc
TimeEmit <LiamNorris1231853211@gmail.com>
2024-06-11 10:24:29 -0700

    Granular error messaging

--- web/src/components/admin/users/BulkAdd.tsx
+++ web/src/components/admin/users/BulkAdd.tsx
@@ -1,7 +1,7 @@
 "use client";
 import useSWRMutation from "swr/mutation";
 import { RobotIcon } from "@/components/icons/icons";
-import { Formik, Form, Field } from "formik";
+import { withFormik, FormikProps, FormikErrors, Form, Field } from "formik";
 
 import { BackButton } from "@/components/BackButton";
 import { Card } from "@tremor/react";
@@ -9,36 +9,75 @@ import { AdminPageTitle } from "@/components/admin/Title";
 import { fetchAssistantEditorInfoSS } from "@/lib/assistants/fetchPersonaEditorInfoSS";
 import { Button, Text } from "@tremor/react";
 
+const WHITESPACE_SPLIT = /\s+/;
+const EMAIL_REGEX = /[^@]+@[^.]+\.[^.]/;
+
 const addUsers = async (url: string, { arg }: { arg: Array<string> }) => {
   return await fetch(url, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ users: arg }),
+    body: JSON.stringify({ emails: arg }),
   });
 };
 
+interface FormProps {}
+
+interface FormValues {
+  emails: string;
+}
+
+const AddUserFormRenderer = ({
+  touched,
+  errors,
+  isSubmitting,
+}: FormikProps<FormValues>) => (
+  <Form>
+    <div className="flex flex-col gap-y-4">
+      <Field id="emails" name="emails" as="textarea" className="p-4" />
+      {touched.emails && errors.emails && (
+        <div className="text-error text-sm">{errors.emails}</div>
+      )}
+      <Button
+        className="mx-auto"
+        color="green"
+        size="md"
+        type="submit"
+        disabled={isSubmitting}
+      >
+        Add!
+      </Button>
+    </div>
+  </Form>
+);
+
+const AddUserForm = withFormik<FormProps, FormValues>({
+  mapPropsToValues: (props) => {
+    return {
+      emails: "",
+    };
+  },
+  validate: (values: FormValues): FormikErrors<FormValues> => {
+    const emails = values.emails.split(WHITESPACE_SPLIT);
+    if (!emails.some(Boolean)) {
+      return { emails: "Required" };
+    }
+    for (let email of emails) {
+      if (!email.match(EMAIL_REGEX)) {
+        return { emails: `${email} is not a valid email` };
+      }
+    }
+    return { emails: "" };
+  },
+  handleSubmit: async (values) => {
+    const emails = values.emails.split(WHITESPACE_SPLIT);
+    await addUsers("/api/manage/admin/users", { arg: emails });
+  },
+})(AddUserFormRenderer);
+
 const BulkAdd = () => {
-  const { trigger } = useSWRMutation("/api/manage/admin/users", addUsers);
-  return (
-    <Formik
-      initialValues={{ emails: "" }}
-      onSubmit={async (values) => {
-        const emails = values.emails.split(/\s+/);
-        await trigger(emails);
-      }}
-    >
-      <Form>
-        <div className="flex flex-col gap-y-4">
-          <Field id="emails" name="emails" as="textarea" className="p-4" />
-          <Button className="mx-auto" color="green" size="md" type="submit">
-            Add!
-          </Button>
-        </div>
-      </Form>
-    </Formik>
-  );
+  return <AddUserForm />;
 };
 
 export default BulkAdd;
