715075c616a640a6ecf23649db95474aedeb8da7
rashad-danswer <rashad@danswer.ai>
2024-06-20 12:47:46 -0700

    fixed alembic stuff

--- backend/alembic/versions/4505fd7302e1_added_is_internet_to_dbdoc.py
+++ backend/alembic/versions/4505fd7302e1_added_is_internet_to_dbdoc.py
@@ -1,7 +1,7 @@
 """added is_internet to DBDoc
 
 Revision ID: 4505fd7302e1
-Revises: 48d14957fe80
+Revises: e209dc5a8156
 Create Date: 2024-06-18 20:46:09.095034
 
 """
@@ -10,14 +10,16 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "4505fd7302e1"
-down_revision = "48d14957fe80"
-branch_labels = None
-depends_on = None
+down_revision = "e209dc5a8156"
 
 
 def upgrade() -> None:
     op.add_column("search_doc", sa.Column("is_internet", sa.Boolean(), nullable=True))
+    op.add_column(
+        "tool", sa.Column("display_name", sa.String(), nullable=True)
+    )
 
 
 def downgrade() -> None:
+    op.drop_column("tool", "display_name")
     op.drop_column("search_doc", "is_internet")
