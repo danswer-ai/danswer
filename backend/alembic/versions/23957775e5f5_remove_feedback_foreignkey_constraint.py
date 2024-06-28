778295fa289ceb6aad0efee9cd374d477d13d10a
hagen-danswer <hagen@danswer.ai>
2024-06-28 14:40:03 -0700

    fixed alembic version

--- backend/alembic/versions/23957775e5f5_remove_feedback_foreignkey_constraint.py
+++ backend/alembic/versions/23957775e5f5_remove_feedback_foreignkey_constraint.py
@@ -16,29 +16,31 @@ depends_on = None  # type: ignore
 
 
 def upgrade() -> None:
-    with op.batch_alter_table("chat_feedback", schema=None) as batch_op:
-        batch_op.drop_constraint("chat_feedback__chat_message_fk", type_="foreignkey")
-        batch_op.create_foreign_key(
-            "chat_feedback__chat_message_fk",
-            "chat_message",
-            ["chat_message_id"],
-            ["id"],
-            ondelete="SET NULL",
-        )
+    op.drop_constraint(
+        "chat_feedback__chat_message_fk", "chat_feedback", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        "chat_feedback__chat_message_fk",
+        "chat_feedback",
+        "chat_message",
+        ["chat_message_id"],
+        ["id"],
+        ondelete="SET NULL",
+    )
     op.alter_column(
         "chat_feedback", "chat_message_id", existing_type=sa.Integer(), nullable=True
     )
-    with op.batch_alter_table("document_retrieval_feedback", schema=None) as batch_op:
-        batch_op.drop_constraint(
-            "document_retrieval_feedback__chat_message_fk", type_="foreignkey"
-        )
-        batch_op.create_foreign_key(
-            "document_retrieval_feedback__chat_message_fk",
-            "chat_message",
-            ["chat_message_id"],
-            ["id"],
-            ondelete="SET NULL",
-        )
+    op.drop_constraint(
+        "document_retrieval__chat_message_fk", "document_retrieval", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        "document_retrieval__chat_message_fk",
+        "document_retrieval",
+        "chat_message",
+        ["chat_message_id"],
+        ["id"],
+        ondelete="SET NULL",
+    )
     op.alter_column(
         "document_retrieval_feedback",
         "chat_message_id",
@@ -48,30 +50,33 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    with op.batch_alter_table("chat_feedback", schema=None) as batch_op:
-        batch_op.drop_constraint("chat_feedback__chat_message_fk", type_="foreignkey")
-        batch_op.create_foreign_key(
-            "chat_feedback__chat_message_fk",
-            "chat_message",
-            ["chat_message_id"],
-            ["id"],
-        )
     op.alter_column(
         "chat_feedback", "chat_message_id", existing_type=sa.Integer(), nullable=False
     )
-    with op.batch_alter_table("document_retrieval_feedback", schema=None) as batch_op:
-        batch_op.drop_constraint(
-            "document_retrieval_feedback__chat_message_fk", type_="foreignkey"
-        )
-        batch_op.create_foreign_key(
-            "document_retrieval_feedback__chat_message_fk",
-            "chat_message",
-            ["chat_message_id"],
-            ["id"],
-        )
+    op.drop_constraint(
+        "chat_feedback__chat_message_fk", "chat_feedback", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        "chat_feedback__chat_message_fk",
+        "chat_feedback",
+        "chat_message",
+        ["chat_message_id"],
+        ["id"],
+    )
+
     op.alter_column(
         "document_retrieval_feedback",
         "chat_message_id",
         existing_type=sa.Integer(),
         nullable=False,
+    )
+    op.drop_constraint(
+        "document_retrieval__chat_message_fk", "document_retrieval", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        "document_retrieval__chat_message_fk",
+        "document_retrieval",
+        "chat_message",
+        ["chat_message_id"],
+        ["id"],
     )
