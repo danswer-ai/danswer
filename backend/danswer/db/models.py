2052f6a2b96294af09d8d76ad4fe63b3bccdfd83
Luke Wulf <luke@danswer.ai>
2024-06-27 18:01:09 -0700

    small fix

--- backend/danswer/db/models.py
+++ backend/danswer/db/models.py
@@ -971,7 +971,7 @@ class Persona(Base):
     )
     # Number of chunks to pass to the LLM for generation.
     num_chunks: Mapped[float | None] = mapped_column(Float, nullable=True)
-    # Number of days old for last_updated documents, only used if use_recent_documents is true
+    # Number of days old for last_updated documents, not used if null
     num_days: Mapped[float | None] = mapped_column(Float, nullable=True)
     # Pass every chunk through LLM for evaluation, fairly expensive
     # Can be turned off globally by admin, in which case, this setting is ignored
