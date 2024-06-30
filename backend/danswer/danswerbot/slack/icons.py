52c3a5e9d22d4c28ab6c491892f75e9db56ab20a
Weves <chrisweaver101@gmail.com>
2024-06-30 13:41:54 -0700

    Fix slackbot citation images

--- backend/danswer/danswerbot/slack/icons.py
+++ backend/danswer/danswerbot/slack/icons.py
@@ -4,9 +4,9 @@ from danswer.configs.constants import DocumentSource
 def source_to_github_img_link(source: DocumentSource) -> str | None:
     # TODO: store these images somewhere better
     if source == DocumentSource.WEB.value:
-        return "https://raw.githubusercontent.com/danswer-ai/danswer/improve-slack-flow/backend/slackbot_images/Web.png"
+        return "https://raw.githubusercontent.com/danswer-ai/danswer/main/backend/slackbot_images/Web.png"
     if source == DocumentSource.FILE.value:
-        return "https://raw.githubusercontent.com/danswer-ai/danswer/improve-slack-flow/backend/slackbot_images/File.png"
+        return "https://raw.githubusercontent.com/danswer-ai/danswer/main/backend/slackbot_images/File.png"
     if source == DocumentSource.GOOGLE_SITES.value:
         return "https://raw.githubusercontent.com/danswer-ai/danswer/main/web/public/GoogleSites.png"
     if source == DocumentSource.SLACK.value:
@@ -20,13 +20,13 @@ def source_to_github_img_link(source: DocumentSource) -> str | None:
     if source == DocumentSource.GITLAB.value:
         return "https://raw.githubusercontent.com/danswer-ai/danswer/main/web/public/Gitlab.png"
     if source == DocumentSource.CONFLUENCE.value:
-        return "https://raw.githubusercontent.com/danswer-ai/danswer/improve-slack-flow/backend/slackbot_images/Confluence.png"
+        return "https://raw.githubusercontent.com/danswer-ai/danswer/main/backend/slackbot_images/Confluence.png"
     if source == DocumentSource.JIRA.value:
-        return "https://raw.githubusercontent.com/danswer-ai/danswer/improve-slack-flow/backend/slackbot_images/Jira.png"
+        return "https://raw.githubusercontent.com/danswer-ai/danswer/main/backend/slackbot_images/Jira.png"
     if source == DocumentSource.NOTION.value:
         return "https://raw.githubusercontent.com/danswer-ai/danswer/main/web/public/Notion.png"
     if source == DocumentSource.ZENDESK.value:
-        return "https://raw.githubusercontent.com/danswer-ai/danswer/improve-slack-flow/backend/slackbot_images/Zendesk.png"
+        return "https://raw.githubusercontent.com/danswer-ai/danswer/main/backend/slackbot_images/Zendesk.png"
     if source == DocumentSource.GONG.value:
         return "https://raw.githubusercontent.com/danswer-ai/danswer/main/web/public/Gong.png"
     if source == DocumentSource.LINEAR.value:
@@ -38,7 +38,7 @@ def source_to_github_img_link(source: DocumentSource) -> str | None:
     if source == DocumentSource.ZULIP.value:
         return "https://raw.githubusercontent.com/danswer-ai/danswer/main/web/public/Zulip.png"
     if source == DocumentSource.GURU.value:
-        return "https://raw.githubusercontent.com/danswer-ai/danswer/improve-slack-flow/backend/slackbot_images/Guru.png"
+        return "https://raw.githubusercontent.com/danswer-ai/danswer/main/backend/slackbot_images/Guru.png"
     if source == DocumentSource.HUBSPOT.value:
         return "https://raw.githubusercontent.com/danswer-ai/danswer/main/web/public/HubSpot.png"
     if source == DocumentSource.DOCUMENT360.value:
@@ -51,8 +51,8 @@ def source_to_github_img_link(source: DocumentSource) -> str | None:
         return "https://raw.githubusercontent.com/danswer-ai/danswer/main/web/public/Sharepoint.png"
     if source == DocumentSource.REQUESTTRACKER.value:
         # just use file icon for now
-        return "https://raw.githubusercontent.com/danswer-ai/danswer/improve-slack-flow/backend/slackbot_images/File.png"
+        return "https://raw.githubusercontent.com/danswer-ai/danswer/main/backend/slackbot_images/File.png"
     if source == DocumentSource.INGESTION_API.value:
-        return "https://raw.githubusercontent.com/danswer-ai/danswer/improve-slack-flow/backend/slackbot_images/File.png"
+        return "https://raw.githubusercontent.com/danswer-ai/danswer/main/backend/slackbot_images/File.png"
 
-    return "https://raw.githubusercontent.com/danswer-ai/danswer/improve-slack-flow/backend/slackbot_images/File.png"
+    return "https://raw.githubusercontent.com/danswer-ai/danswer/main/backend/slackbot_images/File.png"
