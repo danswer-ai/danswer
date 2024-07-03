7e87f163e698b31295009fe702bdda35e1968f7a
pablodanswer <pablo@danswer.ai>
2024-07-02 17:10:10 -0700

    formatting

--- backend/danswer/llm/answering/stream_processing/citation_processing.py
+++ backend/danswer/llm/answering/stream_processing/citation_processing.py
@@ -29,90 +29,78 @@ def extract_citations_from_stream(
     llm_out = ""
     max_citation_num = len(context_docs)
     curr_segment = ""
-    prepend_bracket = False
     cited_inds = set()
     hold = ""
+    last_cited_num = None
+
     for raw_token in tokens:
         if stop_stream:
             next_hold = hold + raw_token
-
             if stop_stream in next_hold:
                 break
-
             if next_hold == stop_stream[: len(next_hold)]:
                 hold = next_hold
                 continue
-
             token = next_hold
             hold = ""
         else:
             token = raw_token
 
-        # Special case of [1][ where ][ is a single token
-        # This is where the model attempts to do consecutive citations like [1][2]
-        if prepend_bracket:
-            curr_segment += "[" + curr_segment
-            prepend_bracket = False
-
         curr_segment += token
         llm_out += token
 
-        possible_citation_pattern = r"(\[\d*$)"  # [1, [, etc
-        possible_citation_found = re.search(possible_citation_pattern, curr_segment)
+        citation_pattern = r"\[(\d+)\]"
+        citations_found = list(re.finditer(citation_pattern, curr_segment))
 
-        citation_pattern = r"\[(\d+)\]"  # [1], [2] etc
-        citation_found = re.search(citation_pattern, curr_segment)
+        if citations_found and not in_code_block(llm_out):
+            last_citation_end = 0
+            for citation in citations_found:
+                numerical_value = int(citation.group(1))
+                if 1 <= numerical_value <= max_citation_num:
+                    context_llm_doc = context_docs[numerical_value - 1]
+                    target_citation_num = doc_id_to_rank_map[
+                        context_llm_doc.document_id
+                    ]
 
-        if citation_found and not in_code_block(llm_out):
-            numerical_value = int(citation_found.group(1))
-            if 1 <= numerical_value <= max_citation_num:
-                context_llm_doc = context_docs[
-                    numerical_value - 1
-                ]  # remove 1 index offset
+                    # Skip consecutive citations of the same work
+                    if target_citation_num == last_cited_num:
+                        start, end = citation.span()
+                        curr_segment = curr_segment[:start] + curr_segment[end:]
+                        continue
 
-                link = context_llm_doc.link
-                target_citation_num = doc_id_to_rank_map.order_mapping[
-                    context_llm_doc.document_id
-                ]
+                    link = context_llm_doc.link
 
-                # Use the citation number for the document's rank in
-                # the search (or selected docs) results
-                curr_segment = re.sub(
-                    rf"\[{numerical_value}\]", f"[{target_citation_num}]", curr_segment
-                )
-
-                if target_citation_num not in cited_inds:
-                    cited_inds.add(target_citation_num)
-                    yield CitationInfo(
-                        citation_num=target_citation_num,
-                        document_id=context_llm_doc.document_id,
+                    # Replace the citation in the current segment
+                    start, end = citation.span()
+                    curr_segment = (
+                        curr_segment[:start]
+                        + f"[{target_citation_num}]"
+                        + curr_segment[end:]
                     )
 
-                if link:
-                    curr_segment = re.sub(r"\[", "[[", curr_segment, count=1)
-                    curr_segment = re.sub("]", f"]]({link})", curr_segment, count=1)
+                    if target_citation_num not in cited_inds:
+                        cited_inds.add(target_citation_num)
+                        yield CitationInfo(
+                            citation_num=target_citation_num,
+                            document_id=context_llm_doc.document_id,
+                        )
 
-                # In case there's another open bracket like [1][, don't want to match this
-            possible_citation_found = None
+                    if link:
+                        curr_segment = (
+                            curr_segment[:start]
+                            + f"[[{target_citation_num}]]({link})"
+                            + curr_segment[end:]
+                        )
 
-        # if we see "[", but haven't seen the right side, hold back - this may be a
-        # citation that needs to be replaced with a link
-        if possible_citation_found:
-            continue
+                    last_citation_end = end
+                    last_cited_num = target_citation_num
 
-        # Special case with back to back citations [1][2]
-        if curr_segment and curr_segment[-1] == "[":
-            curr_segment = curr_segment[:-1]
-            prepend_bracket = True
-
-        yield DanswerAnswerPiece(answer_piece=curr_segment)
-        curr_segment = ""
+            if last_citation_end > 0:
+                yield DanswerAnswerPiece(answer_piece=curr_segment[:last_citation_end])
+                curr_segment = curr_segment[last_citation_end:]
 
     if curr_segment:
-        if prepend_bracket:
-            yield DanswerAnswerPiece(answer_piece="[" + curr_segment)
-        else:
-            yield DanswerAnswerPiece(answer_piece=curr_segment)
+        yield DanswerAnswerPiece(answer_piece=curr_segment)
 
 
 def build_citation_processor(
