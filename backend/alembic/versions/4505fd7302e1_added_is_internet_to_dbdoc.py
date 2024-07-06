34d8574f96c7d0c262396956eea9e3ae215bd180
Weves <chrisweaver101@gmail.com>
2024-07-06 16:20:31 -0700

    Fix migration

--- backend/alembic/versions/4505fd7302e1_added_is_internet_to_dbdoc.py
+++ backend/alembic/versions/4505fd7302e1_added_is_internet_to_dbdoc.py
@@ -1,7 +1,7 @@
 """added is_internet to DBDoc
 
 Revision ID: 4505fd7302e1
-Revises: e209dc5a8156
+Revises: c18cdf4b497e
 Create Date: 2024-06-18 20:46:09.095034
 
 """
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "4505fd7302e1"
-down_revision = "e209dc5a8156"
+down_revision = "c18cdf4b497e"
 
 
 def upgrade() -> None:
