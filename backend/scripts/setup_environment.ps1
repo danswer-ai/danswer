aaaf2db3c1d3d4bddd39d4a9119517ffa7086180
SchadenKai <kairustecsonpersonal@gmail.com>
2024-07-11 15:13:50 +0800

    modified the setup script to install every dependencies

--- backend/scripts/setup_environment.ps1
+++ backend/scripts/setup_environment.ps1
@@ -4,4 +4,6 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 pip install -r ConversationalHealthPlatform/backend/requirements/default.txt
 pip install -r ConversationalHealthPlatform/backend/requirements/dev.txt
 pip install -r ConversationalHealthPlatform/backend/requirements/model_server.txt
+pip install -r ConversationalHealthPlatform/backend/requirements/ee.txt
+pip install -r ConversationalHealthPlatform/backend/requirements/cdk.txt
 cd ConversationalHealthPlatform
