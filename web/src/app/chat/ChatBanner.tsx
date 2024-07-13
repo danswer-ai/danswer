c4f092192469d97425fda3693f83e31229b0e71f
pablodanswer <pablo@danswer.ai>
2024-07-13 08:31:53 -0700

    squash

--- web/src/app/chat/ChatBanner.tsx
+++ web/src/app/chat/ChatBanner.tsx
@@ -16,7 +16,7 @@ export function ChatBanner() {
       className={`
       z-[39] 
       h-[30px]
-      bg-background-custom-header
+      bg-background-weak
       shadow-sm
       m-2
       rounded
