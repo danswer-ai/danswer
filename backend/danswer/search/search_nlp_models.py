017af052be9ceed0498f96b38352c6c356d6feb0
Yuhong Sun <yuhongsun96@gmail.com>
2024-07-14 11:37:10 -0700

    Global Tokenizer Fix (#1825)

--- backend/danswer/search/search_nlp_models.py
+++ backend/danswer/search/search_nlp_models.py
@@ -40,24 +40,21 @@ def clean_model_name(model_str: str) -> str:
     return model_str.replace("/", "_").replace("-", "_").replace(".", "_")
 
 
-# NOTE: If None is used, it may not be using the "correct" tokenizer, for cases
-# where this is more important, be sure to refresh with the actual model name
-def get_default_tokenizer(model_name: str | None = None) -> "AutoTokenizer":
+# NOTE: If no model_name is specified, it may not be using the "correct" tokenizer
+# for cases where this is more important, be sure to refresh with the actual model name
+# One case where it is not particularly important is in the document chunking flow,
+# they're basically all using the sentencepiece tokenizer and whether it's cased or
+# uncased does not really matter, they'll all generally end up with the same chunk lengths.
+def get_default_tokenizer(model_name: str = DOCUMENT_ENCODER_MODEL) -> "AutoTokenizer":
     # NOTE: doing a local import here to avoid reduce memory usage caused by
     # processes importing this file despite not using any of this
     from transformers import AutoTokenizer  # type: ignore
 
     global _TOKENIZER
-    if _TOKENIZER[0] is None or (
-        _TOKENIZER[1] is not None and _TOKENIZER[1] != model_name
-    ):
+    if _TOKENIZER[0] is None or _TOKENIZER[1] != model_name:
         if _TOKENIZER[0] is not None:
             del _TOKENIZER
             gc.collect()
-
-        if model_name is None:
-            # This could be inaccurate
-            model_name = DOCUMENT_ENCODER_MODEL
 
         _TOKENIZER = (AutoTokenizer.from_pretrained(model_name), model_name)
 
@@ -184,6 +181,7 @@ def warm_up_encoders(
         "https://docs.danswer.dev/quickstart"
     )
 
+    # May not be the exact same tokenizer used for the indexing flow
     get_default_tokenizer(model_name=model_name)(warm_up_str)
 
     embed_model = EmbeddingModel(
