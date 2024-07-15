308678aaebd95fd5d859b2d63703d648dd36616b
SchadenKai <kairustecsonpersonal@gmail.com>
2024-07-16 07:47:35 +0800

    temporarily disable landing page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,57 +1,3 @@
-/* import { fetchSettingsSS } from "@/components/settings/lib";
-import { redirect } from "next/navigation";
-
-export default async function Page() {
-  const settings = await fetchSettingsSS();
-
-  if (!settings) {
-    redirect("/search");
-  }
-
-  if (settings.settings.default_page === "search") {
-    redirect("/search");
-  } else {
-    redirect("/chat");
-  }
-} */
-
-/* import { fetchSettingsSS } from "@/components/settings/lib";
-import { redirect } from "next/navigation";
-import CallToActions from "./homepage/callToActions";
-import Certifications from "./homepage/certifications";
-import DataLoaders from "./homepage/dataLoaders";
-import Enterprise from "./homepage/enterprise";
-import Footer from "./homepage/footer";
-import Hero from "./homepage/hero";
-import Models from "./homepage/models";
-import Navbar from "./homepage/navbar";
-import Platform from "./homepage/platform";
-import UseCases from "./homepage/useCases";
-
-export default async function Page() {
-  const settings = await fetchSettingsSS();
-
-  if (!settings) {
-    redirect("/chat");
-  }
-
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <UseCases />
-      <Platform />
-      <Models />
-      <Enterprise />
-      <Certifications />
-      <DataLoaders />
-      <CallToActions />
-      <Footer />
-    </>
-  );
-} */
-"use client";
-
 import { fetchSettingsSS } from "@/components/settings/lib";
 import { redirect } from "next/navigation";
 import CallToActions from "./homepage/callToActions";
@@ -67,23 +13,31 @@ import UseCases from "./homepage/useCases";
 import { useEffect } from "react";
 import Lenis from "lenis";
 
-export default function Page() {
-  /*   const settings = await fetchSettingsSS();
+export default async function Page() {
+  const settings = await fetchSettingsSS();
 
+  // The default page setting must only be available to authenticated users
+  // If the user is not authenticated, they must be redirected to the landing page
   if (!settings) {
     redirect("/chat");
-  } */
+  }
 
-  useEffect(() => {
-    const lenis = new Lenis();
+  if (settings.settings.default_page === "search") {
+    redirect("/search");
+  } else {
+    redirect("/chat");
+  }
 
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+  // useEffect(() => {
+  //   const lenis = new Lenis();
 
-    requestAnimationFrame(raf);
-  }, []);
+  //   function raf(time: number) {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
+
+  //   requestAnimationFrame(raf);
+  // }, []);
 
   return (
     <>
