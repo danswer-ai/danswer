80790b576b7b9393093b485e9117a5d3049875aa
hagen-danswer <hagen@danswer.ai>
2024-07-15 12:24:25 -0700

    will work for the next 76 years

--- backend/tests/regression/answer_quality/cli_utils.py
+++ backend/tests/regression/answer_quality/cli_utils.py
@@ -181,7 +181,7 @@ def cleanup_docker(run_suffix: str) -> None:
 
     containers = [json.loads(line) for line in stdout.splitlines()]
     if not run_suffix:
-        run_suffix = "_"
+        run_suffix = "_20"
     project_name = f"danswer-stack{run_suffix}"
     containers_to_delete = [
         c for c in containers if c["Names"].startswith(project_name)
