e8233928140d352e775bdd8c43fede1cd8881ec7
SchadenKai <kairustecsonpersonal@gmail.com>
2024-07-16 09:23:33 +0800

    added metadata for google sheets and removed other removed connectors

--- web/src/lib/sources.ts
+++ web/src/lib/sources.ts
@@ -1,8 +1,5 @@
 import {
-  BookstackIcon,
-  ClickupIcon,
   ConfluenceIcon,
-  DiscourseIcon,
   DropboxIcon,
   GithubIcon,
   GitlabIcon,
@@ -10,7 +7,6 @@ import {
   GmailIcon,
   GoogleDriveIcon,
   GoogleSitesIcon,
-  GuruIcon,
   HubSpotIcon,
   JiraIcon,
   NotionIcon,
@@ -18,18 +14,17 @@ import {
   SharepointIcon,
   TeamsIcon,
   ZendeskIcon,
-  MediaWikiIcon,
-  WikipediaIcon,
   FileIcon,
   S3Icon,
   R2Icon,
   GoogleStorageIcon,
   OCIStorageIcon,
+  SalesforceIcon,
+  GoogleSheetsIcon
 } from "@/components/icons/icons";
 import { ValidSources } from "./types";
 import { SourceCategory, SourceMetadata } from "./search/interfaces";
 import { Persona } from "@/app/admin/assistants/interfaces";
-import internal from "stream";
 
 interface PartialSourceMetadata {
   icon: React.FC<{ size?: number; className?: string }>;
@@ -92,26 +87,16 @@ const SOURCE_METADATA_MAP: SourceMap = {
     displayName: "Productboard",
     category: SourceCategory.AppConnection,
   },
-  guru: {
-    icon: GuruIcon,
-    displayName: "Guru",
-    category: SourceCategory.AppConnection,
-  },
   hubspot: {
     icon: HubSpotIcon,
     displayName: "HubSpot",
     category: SourceCategory.AppConnection,
   },
-  bookstack: {
-    icon: BookstackIcon,
-    displayName: "BookStack",
-    category: SourceCategory.AppConnection,
-  },
-  google_sites: {
-    icon: GoogleSitesIcon,
-    displayName: "Google Sites",
-    category: SourceCategory.ImportedKnowledge,
-  },
+  // google_sites: {
+  //   icon: GoogleSitesIcon,
+  //   displayName: "Google Sites",
+  //   category: SourceCategory.ImportedKnowledge,
+  // },
   dropbox: {
     icon: DropboxIcon,
     displayName: "Dropbox",
@@ -127,56 +112,41 @@ const SOURCE_METADATA_MAP: SourceMap = {
     displayName: "Teams",
     category: SourceCategory.AppConnection,
   },
-  discourse: {
-    icon: DiscourseIcon,
-    displayName: "Discourse",
-    category: SourceCategory.AppConnection,
-  },
-  wikipedia: {
-    icon: WikipediaIcon,
-    displayName: "Wikipedia",
-    category: SourceCategory.AppConnection,
-  },
-  mediawiki: {
-    icon: MediaWikiIcon,
-    displayName: "MediaWiki",
-    category: SourceCategory.AppConnection,
-  },
-  clickup: {
-    icon: ClickupIcon,
-    displayName: "Clickup",
-    category: SourceCategory.AppConnection,
-  },
   salesforce: {
-    icon: GlobeIcon,
+    icon: SalesforceIcon,
     displayName: "Salesforce",
     category: SourceCategory.AppConnection,
   },
   file: {
     icon: FileIcon,
     displayName: "File",
-    category: SourceCategory.AppConnection,
+    category: SourceCategory.ImportedKnowledge,
   },
   s3: {
     icon: S3Icon,
-    displayName: "File",
-    category: SourceCategory.AppConnection,
+    displayName: "AWS S3",
+    category: SourceCategory.ImportedKnowledge,
   },
   r2: {
     icon: R2Icon,
-    displayName: "File",
-    category: SourceCategory.AppConnection,
+    displayName: "Cloudflare R2",
+    category: SourceCategory.ImportedKnowledge,
   },
   google_cloud_storage: {
     icon: GoogleStorageIcon,
-    displayName: "File",
-    category: SourceCategory.AppConnection,
+    displayName: "Google Storage",
+    category: SourceCategory.ImportedKnowledge,
   },
   oci_storage: {
     icon: OCIStorageIcon,
-    displayName: "File",
-    category: SourceCategory.AppConnection,
-  }
+    displayName: "Oracle Storage",
+    category: SourceCategory.ImportedKnowledge,
+  },
+  google_sheets: {
+    icon: GoogleSheetsIcon,
+    displayName: "Google Sheets",
+    category: SourceCategory.ComingSoon,
+  },
 };
 
 function fillSourceMetadata(
