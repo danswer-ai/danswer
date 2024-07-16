9d97ec66b589b6720f28cd204b7ee19eca9f621b
pablodanswer <pablo@danswer.ai>
2024-07-16 09:35:53 -0700

    squash - cleaner citaitons

--- web/src/components/search/results/Citation.tsx
+++ web/src/components/search/results/Citation.tsx
@@ -5,12 +5,15 @@ import { ReactNode } from "react";
 export function Citation({
   children,
   link,
+  index,
 }: {
   link?: string;
-  children: JSX.Element | string | null | ReactNode;
+  children?: JSX.Element | string | null | ReactNode;
+  index?: number;
 }) {
-  const innerText =
-    children && children?.toString().split("[")[1].split("]")[0];
+  const innerText = children
+    ? children?.toString().split("[")[1].split("]")[0]
+    : index;
 
   if (link != "") {
     return (
@@ -24,7 +27,7 @@ export function Citation({
         >
           <span className="group relative -top-1 text-sm text-gray-500 dark:text-gray-400 selection:bg-indigo-300 selection:text-black dark:selection:bg-indigo-900 dark:selection:text-white">
             <span
-              className="inline-flex bg-background-200 group-hover:bg-background-300 items-center justify-center h-4 min-w-4 px-1 text-center text-xs rounded-full border-1 border-gray-400 ring-1 ring-gray-400 divide-gray-300 dark:divide-gray-700 dark:ring-gray-700 dark:border-gray-700 transition duration-150"
+              className="inline-flex bg-background-200 group-hover:bg-background-300 items-center justify-center h-3.5 min-w-3.5 px-1 text-center text-xs rounded-full border-1 border-gray-400 ring-1 ring-gray-400 divide-gray-300 dark:divide-gray-700 dark:ring-gray-700 dark:border-gray-700 transition duration-150"
               data-number="3"
             >
               {innerText}
@@ -36,10 +39,10 @@ export function Citation({
   } else {
     return (
       <CustomTooltip content={<div>This doc doesn&apos;t have a link!</div>}>
-        <div className="cursor-help leading-none inline ml-1 align-middle">
-          <span className="group relative -top-1 text-sm text-gray-500 dark:text-gray-400 selection:bg-indigo-300 selection:text-black dark:selection:bg-indigo-900 dark:selection:text-white">
+        <div className="inline-block cursor-help leading-none inline ml-1 align-middle">
+          <span className="group relative -top-1 text-gray-500 dark:text-gray-400 selection:bg-indigo-300 selection:text-black dark:selection:bg-indigo-900 dark:selection:text-white">
             <span
-              className="inline-flex bg-background-200 group-hover:bg-background-300 items-center justify-center h-4 min-w-4 px-1 text-center text-xs rounded-full border-1 border-gray-400 ring-1 ring-gray-400 divide-gray-300 dark:divide-gray-700 dark:ring-gray-700 dark:border-gray-700 transition duration-150"
+              className="inline-flex bg-background-200 group-hover:bg-background-300 items-center justify-center h-3.5 min-w-3.5 flex-none px-1 text-center text-xs rounded-full border-1 border-gray-400 ring-1 ring-gray-400 divide-gray-300 dark:divide-gray-700 dark:ring-gray-700 dark:border-gray-700 transition duration-150"
               data-number="3"
             >
               {innerText}
