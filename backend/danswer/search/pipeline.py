8991eff50800d252d6199b82f59269626dd9532f
Yuhong Sun <yuhongsun96@gmail.com>
2024-07-17 23:45:44 -0700

    k

--- backend/danswer/search/pipeline.py
+++ backend/danswer/search/pipeline.py
@@ -357,7 +357,6 @@ class SearchPipeline:
 
         results = run_functions_in_parallel(function_calls=functions)
 
-        # Maps the document_id-chunk_id to the
         return {
             next(iter(value)): value[next(iter(value))] for value in results.values()
         }
