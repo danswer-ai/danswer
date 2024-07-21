dbb3b522d141b6fd7821d7fdf5fbd61ece0319f5
pablodanswer <pablo@danswer.ai>
2024-07-21 10:01:59 -0700

    address full doc as well

--- backend/danswer/search/pipeline.py
+++ backend/danswer/search/pipeline.py
@@ -169,7 +169,7 @@ class SearchPipeline:
         above = self.search_query.chunks_above
         below = self.search_query.chunks_below
 
-        functions_with_args: list[tuple[Callable, tuple]] = []
+        # functions_with_args: list[tuple[Callable, tuple]] = []
         expanded_inference_sections = []
 
         # Full doc setting takes priority
@@ -177,29 +177,32 @@ class SearchPipeline:
         if self.search_query.full_doc:
             seen_document_ids = set()
             unique_chunks = []
+            list_inference_chunks: list[InferenceChunk] = []
             # This preserves the ordering since the chunks are retrieved in score order
             for chunk in retrieved_chunks:
                 if chunk.document_id not in seen_document_ids:
                     seen_document_ids.add(chunk.document_id)
                     unique_chunks.append(chunk)
+                    list_inference_chunks.append(chunk)
 
-                    functions_with_args.append(
-                        (
-                            self.document_index.id_based_retrieval,
-                            (
-                                chunk.document_id,
-                                None,  # Start chunk ind
-                                None,  # End chunk ind
-                                # There is no chunk level permissioning, this expansion around chunks
-                                # can be assumed to be safe
-                                IndexFilters(access_control_list=None),
-                            ),
-                        )
-                    )
+                    # NOTE: left in for eventual refactor with Vespa API
+                    # functions_with_args.append(
+                    #     (
+                    #         self.document_index.id_based_retrieval,
+                    #         (
+                    #             chunk.document_id,
+                    #             None,  # Start chunk ind
+                    #             None,  # End chunk ind
+                    #             # There is no chunk level permissioning, this expansion around chunks
+                    #             # can be assumed to be safe
+                    #             IndexFilters(access_control_list=None),
+                    #         ),
+                    #     )
+                    # )
 
-            list_inference_chunks = run_functions_tuples_in_parallel(
-                functions_with_args, allow_failures=False
-            )
+            # list_inference_chunks = run_functions_tuples_in_parallel(
+            #     functions_with_args, allow_failures=False
+            # )
 
             for ind, chunk in enumerate(unique_chunks):
                 inf_chunks = list_inference_chunks[ind]
