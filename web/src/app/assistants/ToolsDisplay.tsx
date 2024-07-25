f61abbc5a038449c3ffe11030927114fe5864585
pablodanswer <pablo@danswer.ai>
2024-07-25 16:02:43 -0700

    single column assistnat tab rework

--- web/src/app/assistants/ToolsDisplay.tsx
+++ web/src/app/assistants/ToolsDisplay.tsx
@@ -3,6 +3,7 @@ import { ToolSnapshot } from "@/lib/tools/interfaces";
 import { FiImage, FiSearch, FiGlobe, FiMoreHorizontal } from "react-icons/fi";
 import { Persona } from "../admin/assistants/interfaces";
 import { CustomTooltip } from "@/components/tooltip/CustomTooltip";
+import { useState } from "react";
 
 export function ToolsDisplay({ tools }: { tools: ToolSnapshot[] }) {
   return (
@@ -39,53 +40,89 @@ export function ToolsDisplay({ tools }: { tools: ToolSnapshot[] }) {
 export function AssistantTools({
   assistant,
   list,
+  hovered,
 }: {
   assistant: Persona;
   list?: boolean;
+  hovered?: boolean;
 }) {
-  const nonDefaultTools = assistant.tools.filter(
-    (tool) => tool.name !== "SearchTool" && tool.name !== "ImageGenerationTool"
-  );
   return (
-    <div className="relative text-xs flex items-center text-subtle">
-      <span className={`${!list ? "font-medium" : "text-subtle text-sm"}`}>
-        Powers:
+    <div className="relative text-xs flex text-subtle">
+      <span
+        className={`${assistant.tools.length > 0 && "py-1"} ${!list ? "font-medium" : "text-subtle text-sm"}`}
+      >
+        Tools:
       </span>{" "}
       {assistant.tools.length == 0 ? (
         <p className="ml-1">None</p>
       ) : (
-        <>
+        <div className="ml-1 flex flex-wrap gap-1">
           {assistant.tools.map((tool, ind) => {
             if (tool.name === "SearchTool") {
-              return <FiSearch key={ind} className="ml-1 h-3 w-3 my-auto" />;
+              return (
+                <div
+                  className={`
+                    px-1.5
+                    py-1
+                    rounded-lg 
+                    border
+                    border-border 
+                    w-fit 
+                    flex
+                    items-center
+                    ${hovered ? "bg-background-300" : list ? "bg-background-125" : "bg-background-100"}
+                    cursor-pointer`}
+                >
+                  <div className="flex gap-x-1">
+                    <FiSearch key={ind} className="ml-1 h-3 w-3 my-auto" />
+                    Search
+                  </div>
+                </div>
+              );
             } else if (tool.name === "ImageGenerationTool") {
-              return <FiImage key={ind} className="ml-1 h-3 w-3 my-auto" />;
+              return (
+                <div
+                  className={`
+                px-1.5
+                py-1
+                rounded-lg 
+                border
+                border-border 
+                w-fit 
+                flex
+                ${hovered ? "bg-background-300" : list ? "bg-background-125" : "bg-background-100"}
+                cursor-pointer`}
+                >
+                  <div className="flex items-center gap-x-1">
+                    <FiImage
+                      key={ind}
+                      className="ml-1 my-auto  h-3 w-3 my-auto"
+                    />
+                    Image Generation
+                  </div>
+                </div>
+              );
+            } else {
+              return (
+                <div
+                  className={`
+              px-1.5
+              py-1
+              rounded-lg 
+              border
+              border-border 
+              w-fit 
+              flex
+              items-center
+              ${hovered ? "bg-background-300" : list ? "bg-background-125" : "bg-background-100"}
+              cursor-pointer`}
+                >
+                  <div className="flex gap-x-1">{tool.name}</div>
+                </div>
+              );
             }
           })}
-
-          {!(nonDefaultTools.length == 0) && (
-            <div className="my-auto flex items-center">
-              <CustomTooltip
-                position="top"
-                content={
-                  <div className="text-xs max-w-2xl w-full text-200">
-                    <p>Additional Tools:</p>
-                    <div className="flex flex-col mt-1 gap-y-2">
-                      {nonDefaultTools.map((tool, ind) => (
-                        <p key={ind}>
-                          {tool.display_name}:
-                          <span className="ml-2">{tool.description}</span>
-                        </p>
-                      ))}
-                    </div>
-                  </div>
-                }
-              >
-                <FiMoreHorizontal className="cursor-pointer ml-1 h-3 w-3 my-auto" />
-              </CustomTooltip>
-            </div>
-          )}
-        </>
+        </div>
       )}
     </div>
   );
--- web/src/app/chat/modal/configuration/AssistantsTab.tsx
+++ web/src/app/chat/modal/configuration/AssistantsTab.tsx
@@ -5,7 +5,7 @@ import { Bubble } from "@/components/Bubble";
 import { AssistantIcon } from "@/components/assistants/AssistantIcon";
 import { getDisplayNameForModel } from "@/lib/hooks";
 import { getFinalLLM } from "@/lib/llm/utils";
-import React from "react";
+import React, { useState } from "react";
 import { FiBookmark, FiPlus } from "react-icons/fi";
 
 interface AssistantsTabProps {
@@ -26,7 +26,7 @@ export function AssistantsTab({
   return (
     <div className="py-4">
       <h3 className="px-4 text-lg font-semibold">Change Assistant</h3>
-      <div className="px-2 pb-2 mx-2 max-h-[500px] overflow-y-scroll my-3 grid grid-cols-2 gap-4">
+      <div className="px-2 pb-2 mx-2 max-h-[500px] overflow-y-scroll my-3 grid grid-cols-1 gap-4">
         {availableAssistants.map((assistant) => (
           <AssistantCard
             key={assistant.id}
@@ -52,31 +52,32 @@ const AssistantCard = ({
   onSelect: (assistant: Persona) => void;
   llmName: string;
 }) => {
-  const nonDefaultTools = assistant.tools.filter(
-    (tool) => tool.name !== "SearchTool" && tool.name !== "ImageGenerationTool"
-  );
-
+  const [hovering, setHovering] = useState(false);
   return (
     <div
+      onClick={() => onSelect(assistant)}
       key={assistant.id}
       className={`
       p-4 
+      cursor-pointer
       border 
+      hover:bg-hover
       shadow-md 
-      ${isSelected ? "border-accent" : "border-border"}
+      rounded
+      rounded-lg
+      border-border
     `}
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
     >
       <div className="flex items-center mb-2">
         <AssistantIcon assistant={assistant} />
         <div className="ml-2 line-clamp-1 ellipsis font-bold text-sm text-emphasis">
           {assistant.name}
         </div>
-        <button className="ml-auto" onClick={() => onSelect(assistant)}>
-          <FiPlus />
-        </button>
       </div>
 
-      <div className="text-xs text-subtle mb-2 mt-2 line-clamp-3  h-[3rem]">
+      <div className="text-xs text-subtle mb-2 mt-2 line-clamp-3 py-1">
         {assistant.description}
       </div>
       <div className="mt-2 flex flex-col gap-y-1">
@@ -99,7 +100,7 @@ const AssistantCard = ({
             assistant.llm_model_version_override || llmName
           )}
         </div>
-        <AssistantTools assistant={assistant} />
+        <AssistantTools hovered={hovering} assistant={assistant} />
       </div>
     </div>
   );
