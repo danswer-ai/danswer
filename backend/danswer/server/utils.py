711ed9fc2e3faeeaeb00e35d3d447908ce61f60e
CharlesBdg <73182771+CharlesBdg@users.noreply.github.com>
2024-07-25 11:54:34 +0200

    Check if Jira server is v8.13 or under, as the API call as to be different

--- backend/danswer/server/utils.py
+++ backend/danswer/server/utils.py
@@ -13,11 +13,14 @@ def mask_string(sensitive_str: str) -> str:
 def mask_credential_dict(credential_dict: dict[str, Any]) -> dict[str, str]:
     masked_creds = {}
     for key, val in credential_dict.items():
-        if not isinstance(val, str):
+        if isinstance(val, bool):
+            masked_creds[key] = str(val)  # Convert boolean to string
+        elif isinstance(val, str):
+            masked_creds[key] = mask_string(val)
+        else:
             raise ValueError(
-                f"Unable to mask credentials of type other than string, cannot process request."
-                f"Recieved type: {type(val)}"
+                f"Unable to mask credentials of type other than string or boolean, cannot process request. "
+                f"Received type: {type(val)}"
             )
 
-        masked_creds[key] = mask_string(val)
     return masked_creds
