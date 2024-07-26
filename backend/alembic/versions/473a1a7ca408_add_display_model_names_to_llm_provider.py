9d8d395d173ba77ef0b34bfec5d6423eced74eba
pablodanswer <pablo@danswer.ai>
2024-07-25 22:47:07 -0700

    update alembic to set defaults

--- backend/alembic/versions/473a1a7ca408_add_display_model_names_to_llm_provider.py
+++ backend/alembic/versions/473a1a7ca408_add_display_model_names_to_llm_provider.py
@@ -16,12 +16,33 @@ down_revision = "325975216eb3"
 branch_labels = None
 depends_on = None
 
+default_models_by_provider = {
+    "openai": ["gpt-4", "gpt-4o", "gpt-4o-mini"],
+    "bedrock": [
+        "meta.llama3-1-70b-instruct-v1:0",
+        "meta.llama3-1-8b-instruct-v1:0",
+        "anthropic.claude-3-opus-20240229-v1:0",
+        "mistral.mistral-large-2402-v1:0",
+        "anthropic.claude-3-5-sonnet-20240620-v1:0",
+    ],
+    "anthropic": ["claude-3-opus-20240229", "claude-3-5-sonnet-20240620"],
+}
+
 
 def upgrade() -> None:
     op.add_column(
         "llm_provider",
         sa.Column("display_model_names", postgresql.ARRAY(sa.String()), nullable=True),
     )
+
+    connection = op.get_bind()
+    for provider, models in default_models_by_provider.items():
+        connection.execute(
+            sa.text(
+                "UPDATE llm_provider SET display_model_names = :models WHERE provider = :provider"
+            ),
+            {"models": models, "provider": provider},
+        )
 
 
 def downgrade() -> None:
