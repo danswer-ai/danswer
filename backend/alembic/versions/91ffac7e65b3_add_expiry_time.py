2a37c95a5e45536cb17311809da81a4cfdd7b472
Yuhong Sun <yuhongsun96@gmail.com>
2024-07-25 18:18:48 -0700

    Types for Migrations (#1944)

--- backend/alembic/versions/91ffac7e65b3_add_expiry_time.py
+++ backend/alembic/versions/91ffac7e65b3_add_expiry_time.py
@@ -1,7 +1,9 @@
 """add expiry time
+
 Revision ID: 91ffac7e65b3
 Revises: bc9771dccadf
 Create Date: 2024-06-24 09:39:56.462242
+
 """
 
 from alembic import op
@@ -10,8 +12,8 @@ import sqlalchemy as sa
 # revision identifiers, used by Alembic.
 revision = "91ffac7e65b3"
 down_revision = "795b20b85b4b"
-branch_labels = None
-depends_on = None
+branch_labels: None = None
+depends_on: None = None
 
 
 def upgrade() -> None:
