60704632e616e24aa8018f94064ce41176a486f5
pablodanswer <pablo@danswer.ai>
2024-07-30 17:30:57 -0700

    uncomment two lines

--- web/src/app/chat/page.tsx
+++ web/src/app/chat/page.tsx
@@ -43,9 +43,9 @@ export default async function Page({
 
   return (
     <>
-      {/* <InstantSSRAutoRefresh /> */}
-      {/* {shouldShowWelcomeModal && <WelcomeModal user={user} />}
-      {!shouldShowWelcomeModal && !shouldDisplaySourcesIncompleteModal && (
+      <InstantSSRAutoRefresh />
+      {shouldShowWelcomeModal && <WelcomeModal user={user} />}
+      {/* {!shouldShowWelcomeModal && !shouldDisplaySourcesIncompleteModal && (
         <ApiKeyModal user={user} />
       )}
       {shouldDisplaySourcesIncompleteModal && (
