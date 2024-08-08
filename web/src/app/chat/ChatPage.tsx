cc3cad74d0847c210446756c4b8c578420e6d525
pablodanswer <pablo@danswer.ai>
2024-08-08 09:43:28 -0700

    improve color choices

--- web/src/app/chat/ChatPage.tsx
+++ web/src/app/chat/ChatPage.tsx
@@ -1283,7 +1283,7 @@ export function ChatPage({
                       )}
 
                       <div
-                        className={`h-full bg-white w-full relative flex-auto transition-margin duration-300 overflow-x-auto mobile:pb-12 desktop:pb-[100px]`}
+                        className={`h-full w-full relative flex-auto transition-margin duration-300 overflow-x-auto mobile:pb-12 desktop:pb-[100px]`}
                         {...getRootProps()}
                       >
                         {/* <input {...getInputProps()} /> */}
