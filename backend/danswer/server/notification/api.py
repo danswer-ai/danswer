b7423481632cf400c39eadb8ead769b70c6a0fc7
Yuhong Sun <yuhongsun96@gmail.com>
2024-08-10 11:12:25 -0700

    k

--- backend/danswer/server/notification/api.py
+++
@@ -1,9 +0,0 @@
-# router = APIRouter(prefix="/notifications")
-# @router.get("")
-# def fetch_notifications(
-#     include_dismissed: bool = False,
-#     db_session: Session = Depends(get_db_session),
-#     user: User = Depends(current_user),
-# ) -> NotificationList:
-#     notifications = get_user_notifications(db_session, user, include_dismissed)
-#     return NotificationList(notifications=[NotificationResponse.from_orm(n) for n in notifications])
