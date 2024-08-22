a52ec689c0bdfd5507655f1c23a2e920b86892fd
SchadenKai <kairustecsonpersonal@gmail.com>
2024-08-22 16:16:24 +0800

    chore: perform prettier locally through pre-commit

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -2,7 +2,9 @@ var merge = require("lodash/merge");
 
 const baseThemes = require("./tailwind-themes/tailwind.config.ts");
 const customThemes = process.env.NEXT_PUBLIC_THEME
-  ? require(`./tailwind-themes/custom/${process.env.NEXT_PUBLIC_THEME}/tailwind.config.ts`)
+  ? require(
+      `./tailwind-themes/custom/${process.env.NEXT_PUBLIC_THEME}/tailwind.config.ts`
+    )
   : null;
 
 /** @type {import('tailwindcss').Config} */
