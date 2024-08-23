2e0222d1c129471ee0c7788ee2219c89319a6e16
pablodanswer <pablo@danswer.ai>
2024-08-23 09:15:50 -0700

    logotype from toggle -> redirect (#2222)

--- web/src/app/chat/shared_chat_search/FixedLogo.tsx
+++ web/src/app/chat/shared_chat_search/FixedLogo.tsx
@@ -4,23 +4,22 @@ import { HeaderTitle } from "@/components/header/HeaderTitle";
 import { Logo } from "@/components/Logo";
 import { SettingsContext } from "@/components/settings/SettingsProvider";
 import { NEXT_PUBLIC_DO_NOT_USE_TOGGLE_OFF_DANSWER_POWERED } from "@/lib/constants";
+import Link from "next/link";
 import { useContext } from "react";
 import { FiSidebar } from "react-icons/fi";
 
-export default function FixedLogo({
-  toggleSidebar = () => null,
-}: {
-  toggleSidebar?: () => void;
-}) {
+export default function FixedLogo() {
   const combinedSettings = useContext(SettingsContext);
   const settings = combinedSettings?.settings;
   const enterpriseSettings = combinedSettings?.enterpriseSettings;
 
   return (
     <>
-      <div
-        onClick={toggleSidebar}
-        className="fixed  cursor-pointer flex z-40 left-2.5 top-2"
+      <Link
+        href={
+          settings && settings.default_page === "chat" ? "/chat" : "/search"
+        }
+        className="fixed cursor-pointer flex z-40 left-2.5 top-2"
       >
         <div className="max-w-[200px] mobile:hidden flex items-center gap-x-1 my-auto">
           <div className="flex-none my-auto">
@@ -39,7 +38,7 @@ export default function FixedLogo({
             )}
           </div>
         </div>
-      </div>
+      </Link>
       <div className="mobile:hidden fixed left-2.5 bottom-4">
         <FiSidebar />
       </div>
