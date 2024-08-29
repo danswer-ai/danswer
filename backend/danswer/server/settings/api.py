da7028b644713681d2816ba34ce0621b4be71c18
Faizal Babu <fazyvmh82@gmail.com>
2024-08-29 14:30:40 +0400

    Added CRUD endpoints for the user info and image url
    Added CRUD endpoints for the user info and image url. Also changed the top-right corner to dispay email id

--- backend/danswer/server/settings/api.py
+++ backend/danswer/server/settings/api.py
@@ -9,6 +9,9 @@ from danswer.server.settings.models import Settings, KeyValueStoreGeneric
 
 from danswer.server.settings.store import load_settings, store_settings, store_key_value, load_key_value, delete_key_value_generic
 
+USER_INFO_KEY = "USER_INFO_"
+IMAGE_URL_KEY = "IMAGE_URL_"
+
 admin_router = APIRouter(prefix="/admin/settings")
 basic_router = APIRouter(prefix="/settings")
 
@@ -29,19 +32,41 @@ def fetch_settings(_: User | None = Depends(current_user)) -> Settings:
     return load_settings()
 
 
-@basic_router.put("/add")
-def upsert_key_value(key1: KeyValueStoreGeneric, _: User | None = Depends(current_user)) -> None:
-    kvstore = KeyValueStoreGeneric(key=key1.key, value=key1.value)
+@basic_router.put("/user_info")
+def upsert_user_info(user_info: KeyValueStoreGeneric, _: User | None = Depends(current_user)) -> None:
+    key = f"{USER_INFO_KEY}{user_info.key}"
+    kvstore = KeyValueStoreGeneric(key=key, value=user_info.value)
     store_key_value(kvstore)
 
 
-@basic_router.get('/{key}')
-def get_key_value(key: str,  _: User | None = Depends(current_user)) -> KeyValueStoreGeneric:
+@basic_router.get('/user_info/{key}')
+def get_user_info(key: str,  _: User | None = Depends(current_user)) -> KeyValueStoreGeneric:
+    key = f"{USER_INFO_KEY}{key}"
     return load_key_value(key)
 
 
-@basic_router.delete('/{key}')
-def delete_key_value(key: str,  _: User | None = Depends(current_user)) -> None:
+@basic_router.delete('/user_info/{key}')
+def delete_user_info(key: str,  _: User | None = Depends(current_user)) -> None:
+    key = f"{USER_INFO_KEY}{key}"
+    delete_key_value_generic(key)
+
+
+@basic_router.put("/image_url")
+def upsert_image_url(user_info: KeyValueStoreGeneric, _: User | None = Depends(current_user)) -> None:
+    key = f"{IMAGE_URL_KEY}{user_info.key}"
+    kvstore = KeyValueStoreGeneric(key=key, value=user_info.value)
+    store_key_value(kvstore)
+
+
+@basic_router.get('/image_url/{key}')
+def get_image_url(key: str,  _: User | None = Depends(current_user)) -> KeyValueStoreGeneric:
+    key = f"{IMAGE_URL_KEY}{key}"
+    return load_key_value(key)
+
+
+@basic_router.delete('/image_url/{key}')
+def delete_image_url(key: str,  _: User | None = Depends(current_user)) -> None:
+    key = f"{IMAGE_URL_KEY}{key}"
     delete_key_value_generic(key)
 
 
