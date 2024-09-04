bd23210503b8fc4a2fb659bbe173111044f34d93
SchadenKai <kairustecsonpersonal@gmail.com>
2024-09-05 05:43:18 +0800

    refactor: show also the public data sources in creating teamspaces

--- web/src/app/ee/admin/teams/ConnectorEditor.tsx
+++ web/src/app/ee/admin/teams/ConnectorEditor.tsx
@@ -15,39 +15,36 @@ export const ConnectorEditor = ({
 }: ConnectorEditorProps) => {
   return (
     <div className="mb-3 flex gap-2 flex-wrap">
-      {allCCPairs
-        // remove public docs, since they don't make sense as part of a group
-        .filter((ccPair) => !ccPair.public_doc)
-        .map((ccPair) => {
-          const ind = selectedCCPairIds.indexOf(ccPair.cc_pair_id);
-          let isSelected = ind !== -1;
-          return (
-            <Badge
-              key={`${ccPair.connector.id}-${ccPair.credential.id}`}
-              className="cursor-pointer"
-              variant={isSelected ? "default" : "outline"}
-              onClick={() => {
-                if (isSelected) {
-                  setSetCCPairIds(
-                    selectedCCPairIds.filter(
-                      (ccPairId) => ccPairId !== ccPair.cc_pair_id
-                    )
-                  );
-                } else {
-                  setSetCCPairIds([...selectedCCPairIds, ccPair.cc_pair_id]);
-                }
-              }}
-            >
-              <ConnectorTitle
-                connector={ccPair.connector}
-                ccPairId={ccPair.cc_pair_id}
-                ccPairName={ccPair.name}
-                isLink={false}
-                showMetadata={false}
-              />
-            </Badge>
-          );
-        })}
+      {allCCPairs.map((ccPair) => {
+        const ind = selectedCCPairIds.indexOf(ccPair.cc_pair_id);
+        let isSelected = ind !== -1;
+        return (
+          <Badge
+            key={`${ccPair.connector.id}-${ccPair.credential.id}`}
+            className="cursor-pointer"
+            variant={isSelected ? "default" : "outline"}
+            onClick={() => {
+              if (isSelected) {
+                setSetCCPairIds(
+                  selectedCCPairIds.filter(
+                    (ccPairId) => ccPairId !== ccPair.cc_pair_id
+                  )
+                );
+              } else {
+                setSetCCPairIds([...selectedCCPairIds, ccPair.cc_pair_id]);
+              }
+            }}
+          >
+            <ConnectorTitle
+              connector={ccPair.connector}
+              ccPairId={ccPair.cc_pair_id}
+              ccPairName={ccPair.name}
+              isLink={false}
+              showMetadata={false}
+            />
+          </Badge>
+        );
+      })}
     </div>
   );
 };
