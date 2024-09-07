138866aa5fd5e70e2efbcff17cab58ef4bcf0dac
Richard Kuo (Danswer) <rkuo@danswer.ai>
2024-09-07 00:18:58 -0700

    missed a file

--- backend/ee/danswer/db/user_group.py
+++ backend/ee/danswer/db/user_group.py
@@ -119,7 +119,7 @@ def fetch_user_groups_for_user(
     return db_session.scalars(stmt).all()
 
 
-def select_documents_by_usergroup(
+def construct_document_select_by_usergroup(
     user_group_id: int,
 ) -> Select:
     """This returns a statement that should be executed using
