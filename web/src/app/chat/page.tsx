45357dc6f999dcc24082014dcd81e5556ec9cf69
ervinpiol <rvnpiol14@gmail.com>
2024-09-07 10:54:42 +0800

    feat: fix welcome modal style

--- web/src/app/chat/page.tsx
+++ web/src/app/chat/page.tsx
@@ -41,8 +41,8 @@ export default async function Page({
   return (
     <>
       <InstantSSRAutoRefresh />
-      {/*  {shouldShowWelcomeModal && <WelcomeModal user={user} />} */}
       {shouldShowWelcomeModal && <WelcomeModal user={user} />}
+
       {!shouldShowWelcomeModal && !shouldDisplaySourcesIncompleteModal && (
         <ApiKeyModal user={user} />
       )}
