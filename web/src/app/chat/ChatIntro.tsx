f53ed89eec0287c62f543c3bcc2b3fb11b7ce532
ervinpiol <rvnpiol14@gmail.com>
2024-09-09 21:41:18 +0800

    chore: remove comment on ChatIntro.tsx

--- web/src/app/chat/ChatIntro.tsx
+++ web/src/app/chat/ChatIntro.tsx
@@ -1,95 +1,3 @@
-/* import { getSourceMetadataForSources } from "@/lib/sources";
-import { ValidSources } from "@/lib/types";
-import { Assistant } from "../admin/assistants/interfaces";
-import { HoverPopup } from "@/components/HoverPopup";
-import { Badge } from "@/components/ui/badge";
-import { Bookmark, Info } from "lucide-react";
-import { CustomTooltip } from "@/components/CustomTooltip";
-
-export function ChatIntro({
-  availableSources,
-  liveAssistant,
-  children,
-}: {
-  availableSources: ValidSources[];
-  liveAssistant: Assistant;
-  children?: React.ReactNode;
-}) {
-  const availableSourceMetadata = getSourceMetadataForSources(availableSources);
-
-  return (
-    <>
-      <div className="flex 2xl:justify-center w-full py-20">
-        <div className="max-w-full 2xl:w-searchbar px-5 2xl:px-0 mt-10 md:mt-16 lg:mt-0 2xl:mt-14">
-          <div className="flex">
-            <div>
-              <h1 className="flex flex-col text-[2.5rem] md:text-[3.5rem] font-semibold leading-[1.2] tracking-tighter">
-                <span className="h1-bg">Hi, I am {liveAssistant?.name},</span>
-                <span className="h1-bg">How can I help you today?</span>
-              </h1>
-            </div>
-          </div>
-
-          {children}
-
-          {liveAssistant && liveAssistant.num_chunks !== 0 && (
-            <div className="pt-4">
-              {liveAssistant.document_sets.length > 0 && (
-                <div className="mt-2">
-                  <p className="mt-4 mb-1 font-bold text-dark-900">
-                    Knowledge Sets:{" "}
-                  </p>
-                  <div className="flex flex-wrap gap-2">
-                    {liveAssistant.document_sets.map((documentSet) => (
-                      <div key={documentSet.id} className="w-fit">
-                        <CustomTooltip
-                          trigger={
-                            <Badge variant="outline">
-                              <Bookmark size={16} />
-                              {documentSet.name}
-                            </Badge>
-                          }
-                        >
-                          <div className="flex items-center gap-1">
-                            <Info size={16} />
-                            <div className="!text-sm">
-                              {" "}
-                              {documentSet.description}
-                            </div>
-                          </div>
-                        </CustomTooltip>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              )}
-
-              {availableSources.length > 0 && (
-                <div className="mt-1">
-                  <p className="mt-4 mb-1 font-bold text-dark-900">
-                    Connected Sources:{" "}
-                  </p>
-                  <div className={`flex flex-wrap gap-2`}>
-                    {availableSourceMetadata.map((sourceMetadata) => (
-                      <Badge
-                        key={sourceMetadata.internalName}
-                        variant="secondary"
-                      >
-                        {sourceMetadata.icon({})}
-                        {sourceMetadata.displayName}
-                      </Badge>
-                    ))}
-                  </div>
-                </div>
-              )}
-            </div>
-          )}
-        </div>
-      </div>
-    </>
-  );
-} */
-
 import { getSourceMetadataForSources } from "@/lib/sources";
 import { User, ValidSources } from "@/lib/types";
 import { Assistant } from "../admin/assistants/interfaces";
@@ -121,11 +29,11 @@ export function ChatIntro({
             <h1 className="text-[1.5rem] md:text-[2.5rem] font-semibold leading-[1.2] tracking-tighter text-dark-900 text-center">
               Fitness, Workout & Diet - PhD Coach
             </h1>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-2">
               <span className="text-subtle">By |</span>
               <p className="text-dark-900">{user?.full_name}</p>
             </div>
-            <p className="text-center text-dark-900 w-3/4">
+            <p className="text-center text-dark-900 w-3/4 line-clamp">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima,
               voluptatum odio? Aspernatur ducimus porro voluptas veniam maiores
               possimus consequuntur debitis dolore dolorum minima saepe, aliquid
@@ -138,14 +46,14 @@ export function ChatIntro({
               {liveAssistant.document_sets.length > 0 && (
                 <div className="mt-2">
                   <p className="mt-4 mb-1 font-bold text-dark-900">
-                    Knowledge Sets:{" "}
+                    Document Sets:{" "}
                   </p>
                   <div className="flex flex-wrap gap-2">
                     {liveAssistant.document_sets.map((documentSet) => (
                       <div key={documentSet.id} className="w-fit">
                         <CustomTooltip
                           trigger={
-                            <Badge variant="outline">
+                            <Badge variant="secondary">
                               <Bookmark size={16} />
                               {documentSet.name}
                             </Badge>
@@ -154,7 +62,6 @@ export function ChatIntro({
                           <div className="flex items-center gap-1">
                             <Info size={16} />
                             <div className="!text-sm">
-                              {" "}
                               {documentSet.description}
                             </div>
                           </div>
