f4f80122d01153fbf9dc98a60a5e7e5d117651ce
SchadenKai <kairustecsonpersonal@gmail.com>
2024-09-12 13:42:12 +0800

    fix: chat and search functionality does not work on terminal; temporarily fix the development env

--- web/next.config.js
+++ web/next.config.js
@@ -21,34 +21,34 @@ const nextConfig = {
       },
     ];
   },
-  redirects: async () => {
-    // In production, something else (nginx in the one box setup) should take
-    // care of this redirect. TODO (chris): better support setups where
-    // web_server and api_server are on different machines.
-    const defaultRedirects = [];
+  // redirects: async () => {
+  //   // In production, something else (nginx in the one box setup) should take
+  //   // care of this redirect. TODO (chris): better support setups where
+  //   // web_server and api_server are on different machines.
+  //   const defaultRedirects = [];
 
-    if (process.env.NODE_ENV === "production") return defaultRedirects;
+  //   if (process.env.NODE_ENV === "production") return defaultRedirects;
 
-    return defaultRedirects.concat([
-      {
-        source: "/api/chat/send-message:params*",
-        destination: "http://127.0.0.1:8080/chat/send-message:params*", // Proxy to Backend
-        permanent: true,
-      },
-      {
-        source: "/api/query/stream-answer-with-quote:params*",
-        destination:
-          "http://127.0.0.1:8080/query/stream-answer-with-quote:params*", // Proxy to Backend
-        permanent: true,
-      },
-      {
-        source: "/api/query/stream-query-validation:params*",
-        destination:
-          "http://127.0.0.1:8080/query/stream-query-validation:params*", // Proxy to Backend
-        permanent: true,
-      },
-    ]);
-  },
+  //   return defaultRedirects.concat([
+  //     {
+  //       source: "/api/chat/send-message:params*",
+  //       destination: "http://127.0.0.1:8080/chat/send-message:params*", // Proxy to Backend
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/api/query/stream-answer-with-quote:params*",
+  //       destination:
+  //         "http://127.0.0.1:8080/query/stream-answer-with-quote:params*", // Proxy to Backend
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/api/query/stream-query-validation:params*",
+  //       destination:
+  //         "http://127.0.0.1:8080/query/stream-query-validation:params*", // Proxy to Backend
+  //       permanent: true,
+  //     },
+  //   ]);
+  // },
   publicRuntimeConfig: {
     version,
   },
