f31551e0ef99998790166a2923cb9a8e82585906
hagen-danswer <hagen@danswer.ai>
2024-09-12 14:43:10 -0700

    comment

--- backend/ee/danswer/server/query_and_chat/models.py
+++ backend/ee/danswer/server/query_and_chat/models.py
@@ -55,6 +55,7 @@ class BasicCreateChatMessageWithHistoryRequest(ChunkContext):
     retrieval_options: RetrievalDetails = Field(default_factory=RetrievalDetails)
     query_override: str | None = None
     skip_rerank: bool | None = None
+    # If search_doc_ids provided, then retrieval options are unused
     search_doc_ids: list[int] | None = None
 
 
