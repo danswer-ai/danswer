837fcf4560d5f053e5e73e932d72a572cc74c38a
Richard Kuo (Danswer) <rkuo@danswer.ai>
2024-09-13 17:34:37 -0700

    fix document set connection removal sync, add tests for document set and user group removal

--- backend/tests/integration/tests/usergroup/test_usergroup_syncing.py
+++ backend/tests/integration/tests/usergroup/test_usergroup_syncing.py
@@ -3,67 +3,12 @@ from tests.integration.common_utils.constants import NUM_DOCS
 from tests.integration.common_utils.managers.api_key import APIKeyManager
 from tests.integration.common_utils.managers.cc_pair import CCPairManager
 from tests.integration.common_utils.managers.document import DocumentManager
-from tests.integration.common_utils.managers.document_set import DocumentSetManager
 from tests.integration.common_utils.managers.user import UserManager
+from tests.integration.common_utils.managers.user_group import UserGroupManager
 from tests.integration.common_utils.test_models import TestAPIKey
 from tests.integration.common_utils.test_models import TestUser
+from tests.integration.common_utils.test_models import TestUserGroup
 from tests.integration.common_utils.vespa import TestVespaClient
-
-
-def test_multiple_document_sets_syncing_same_connnector(
-    reset: None, vespa_client: TestVespaClient
-) -> None:
-    # Creating an admin user (first user created is automatically an admin)
-    admin_user: TestUser = UserManager.create(name="admin_user")
-
-    # add api key to user
-    api_key: TestAPIKey = APIKeyManager.create(
-        user_performing_action=admin_user,
-    )
-
-    # create connector
-    cc_pair_1 = CCPairManager.create_from_scratch(
-        source=DocumentSource.INGESTION_API,
-        user_performing_action=admin_user,
-    )
-
-    # seed documents
-    cc_pair_1 = DocumentManager.seed_and_attach_docs(
-        cc_pair=cc_pair_1,
-        num_docs=NUM_DOCS,
-        api_key=api_key,
-    )
-
-    # Create document sets
-    doc_set_1 = DocumentSetManager.create(
-        cc_pair_ids=[cc_pair_1.id],
-        user_performing_action=admin_user,
-    )
-    doc_set_2 = DocumentSetManager.create(
-        cc_pair_ids=[cc_pair_1.id],
-        user_performing_action=admin_user,
-    )
-
-    DocumentSetManager.wait_for_sync(
-        user_performing_action=admin_user,
-    )
-
-    DocumentSetManager.verify(
-        document_set=doc_set_1,
-        user_performing_action=admin_user,
-    )
-    DocumentSetManager.verify(
-        document_set=doc_set_2,
-        user_performing_action=admin_user,
-    )
-
-    # make sure documents are as expected
-    DocumentManager.verify(
-        vespa_client=vespa_client,
-        cc_pair=cc_pair_1,
-        doc_set_names=[doc_set_1.name, doc_set_2.name],
-        doc_creating_user=admin_user,
-    )
 
 
 def test_removing_connector(reset: None, vespa_client: TestVespaClient) -> None:
@@ -98,60 +43,60 @@ def test_removing_connector(reset: None, vespa_client: TestVespaClient) -> None:
         api_key=api_key,
     )
 
-    # Create document sets
-    doc_set_1 = DocumentSetManager.create(
+    # Create user group
+    user_group_1: TestUserGroup = UserGroupManager.create(
         cc_pair_ids=[cc_pair_1.id, cc_pair_2.id],
         user_performing_action=admin_user,
     )
 
-    DocumentSetManager.wait_for_sync(
+    UserGroupManager.wait_for_sync(
+        user_groups_to_check=[user_group_1], user_performing_action=admin_user
+    )
+
+    UserGroupManager.verify(
+        user_group=user_group_1,
         user_performing_action=admin_user,
     )
 
-    DocumentSetManager.verify(
-        document_set=doc_set_1,
-        user_performing_action=admin_user,
-    )
-
-    # make sure cc_pair_1 docs are doc_set_1 only
+    # make sure cc_pair_1 docs are user_group_1 only
     DocumentManager.verify(
         vespa_client=vespa_client,
         cc_pair=cc_pair_1,
-        doc_set_names=[doc_set_1.name],
+        group_names=[user_group_1.name],
         doc_creating_user=admin_user,
     )
 
-    # make sure cc_pair_2 docs are doc_set_1 only
+    # make sure cc_pair_2 docs are user_group_1 only
     DocumentManager.verify(
         vespa_client=vespa_client,
         cc_pair=cc_pair_2,
-        doc_set_names=[doc_set_1.name],
+        group_names=[user_group_1.name],
         doc_creating_user=admin_user,
     )
 
     # remove cc_pair_2 from document set
-    doc_set_1.cc_pair_ids = [cc_pair_1.id]
-    DocumentSetManager.edit(
-        doc_set_1,
+    user_group_1.cc_pair_ids = [cc_pair_1.id]
+    UserGroupManager.edit(
+        user_group_1,
         user_performing_action=admin_user,
     )
 
-    DocumentSetManager.wait_for_sync(
+    UserGroupManager.wait_for_sync(
         user_performing_action=admin_user,
     )
 
-    # make sure cc_pair_1 docs are doc_set_1 only
+    # make sure cc_pair_1 docs are user_group_1 only
     DocumentManager.verify(
         vespa_client=vespa_client,
         cc_pair=cc_pair_1,
-        doc_set_names=[doc_set_1.name],
+        group_names=[user_group_1.name],
         doc_creating_user=admin_user,
     )
 
-    # make sure cc_pair_2 docs have no doc set
+    # make sure cc_pair_2 docs have no user group
     DocumentManager.verify(
         vespa_client=vespa_client,
         cc_pair=cc_pair_2,
-        doc_set_names=[],
+        group_names=[],
         doc_creating_user=admin_user,
     )
