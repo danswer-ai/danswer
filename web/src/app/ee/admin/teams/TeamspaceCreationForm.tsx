5c4f253a5efc511d92eb244dff043ec8e003da95
ervinpiol <rvnpiol14@gmail.com>
2024-09-16 13:13:02 +0800

    feat: change font styles and padding

--- web/src/app/ee/admin/teams/TeamspaceCreationForm.tsx
+++ web/src/app/ee/admin/teams/TeamspaceCreationForm.tsx
@@ -42,10 +42,6 @@ export const TeamspaceCreationForm = ({
 
   return (
     <div>
-      <h2 className="text-xl font-bold flex">
-        {isUpdate ? "Update a Teamspace" : "Create a new Teamspace"}
-      </h2>
-
       <Formik
         initialValues={{
           name: existingTeamspace ? existingTeamspace.name : "",
@@ -95,46 +91,42 @@ export const TeamspaceCreationForm = ({
                 autoCompleteDisabled={true}
               />
 
-              <h2 className="mb-1 font-medium">
-                Select which connectors this group has access to:
-              </h2>
-              <p className="mb-3 text-xs">
-                All documents indexed by the selected connectors will be visible
-                to users in this group.
-              </p>
-
-              <ConnectorEditor
-                allCCPairs={ccPairs}
-                selectedCCPairIds={values.cc_pair_ids}
-                setSetCCPairIds={(ccPairsIds) =>
-                  setFieldValue("cc_pair_ids", ccPairsIds)
-                }
-              />
-
-              {/* <div>
-                <h2 className="mb-1 font-medium">Assistants</h2>
-                <p className="mb-3 text-xs">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              <div className="pb-4">
+                <h3 className="text-sm pb-1 font-semibold">
+                  Select which connectors this group has access to:
+                </h3>
+                <p className="text-xs pb-2">
+                  All documents indexed by the selected connectors will be
+                  visible to users in this group.
                 </p>
-                <Assistants assistants={assistants} />
-              </div> */}
 
-              <h2 className="mb-1 font-medium">
-                Select which Users should be a part of this Group.
-              </h2>
-              <p className="mb-3 text-xs">
-                All selected users will be able to search through all documents
-                indexed by the selected connectors.
-              </p>
-              <div className="mb-3 gap-2">
-                <UserEditor
-                  selectedUserIds={values.user_ids}
-                  setSelectedUserIds={(userIds) =>
-                    setFieldValue("user_ids", userIds)
+                <ConnectorEditor
+                  allCCPairs={ccPairs}
+                  selectedCCPairIds={values.cc_pair_ids}
+                  setSetCCPairIds={(ccPairsIds) =>
+                    setFieldValue("cc_pair_ids", ccPairsIds)
                   }
-                  allUsers={users}
-                  existingUsers={[]}
                 />
+              </div>
+
+              <div className="pb-4">
+                <h3 className="text-sm pb-1 font-semibold">
+                  Select which Users should be a part of this Group.
+                </h3>
+                <p className="text-xs">
+                  All selected users will be able to search through all
+                  documents indexed by the selected connectors.
+                </p>
+                <div>
+                  <UserEditor
+                    selectedUserIds={values.user_ids}
+                    setSelectedUserIds={(userIds) =>
+                      setFieldValue("user_ids", userIds)
+                    }
+                    allUsers={users}
+                    existingUsers={[]}
+                  />
+                </div>
               </div>
 
               <div className="flex pt-6">
