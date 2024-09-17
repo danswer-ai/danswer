070a70d34f76a929d795563ece4136360d4c8ce5
danswer-trial <danswer-trial@danswer-trials-MacBook-Pro.local>
2024-09-16 19:28:07 -0700

    Fix head conflict

--- backend/alembic/versions/729fa47417b3_standard_answer_categories_to_persona_.py
+++ backend/alembic/versions/729fa47417b3_standard_answer_categories_to_persona_.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "729fa47417b3"
-down_revision = "5c7fdadae813"
+down_revision = "c99d76fcd298"
 branch_labels = None
 depends_on = None
 
