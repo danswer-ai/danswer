01cc98f7b3d5e999dc894c337b37f65f272fcc73
ervinpiol <rvnpiol14@gmail.com>
2024-09-28 11:19:38 +0800

    feat: add badge on the combobox

--- web/src/app/ee/admin/teams/UserEditor.tsx
+++ web/src/app/ee/admin/teams/UserEditor.tsx
@@ -1,7 +1,4 @@
 import { User } from "@/lib/types";
-import { Button } from "@/components/ui/button";
-import { X } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
 import { Combobox } from "@/components/Combobox";
 
 interface UserEditorProps {
@@ -19,10 +16,6 @@ export const UserEditor = ({
   existingUsers,
   onSubmit,
 }: UserEditorProps) => {
-  const selectedUsers = allUsers.filter((user) =>
-    selectedUserIds.includes(user.id)
-  );
-
   const availableUsers = allUsers
     .filter(
       (user) =>
@@ -35,49 +28,11 @@ export const UserEditor = ({
     }));
 
   return (
-    <>
-      <div className="flex">
-        <Combobox
-          items={availableUsers}
-          onSelect={(selectedValue) => {
-            if (selectedValue) {
-              setSelectedUserIds([
-                ...Array.from(new Set([...selectedUserIds, selectedValue])),
-              ]);
-            }
-          }}
-          placeholder="user"
-          label="Select user"
-        />
-
-        {onSubmit && (
-          <Button
-            className="ml-3 flex-nowrap w-32"
-            onClick={() => onSubmit(selectedUsers)}
-          >
-            Add Users
-          </Button>
-        )}
-      </div>
-
-      <div className="flex flex-wrap gap-2 pt-2">
-        {selectedUsers.length > 0 &&
-          selectedUsers.map((selectedUser) => (
-            <Badge
-              key={selectedUser.id}
-              onClick={() => {
-                setSelectedUserIds(
-                  selectedUserIds.filter((userId) => userId !== selectedUser.id)
-                );
-              }}
-              variant="outline"
-              className="cursor-pointer hover:bg-opacity-80"
-            >
-              {selectedUser.email}
-              <X className="ml-1 my-auto cursor-pointer" size={14} />
-            </Badge>
-          ))}
-      </div>
-    </>
+    <Combobox
+      items={availableUsers}
+      onSelect={(selectedValues) => setSelectedUserIds(selectedValues)}
+      placeholder="user"
+      label="Select user"
+    />
   );
 };
