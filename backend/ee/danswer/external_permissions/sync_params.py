fca21873905ee024b6dc2a7c7e28d19117f8e315
hagen-danswer <hagen@danswer.ai>
2024-09-30 07:39:25 -0700

    param tweak

--- backend/ee/danswer/external_permissions/sync_params.py
+++ backend/ee/danswer/external_permissions/sync_params.py
@@ -44,7 +44,7 @@ GROUP_PERMISSIONS_FUNC_MAP: dict[DocumentSource, SyncFuncType] = {
 PERMISSION_SYNC_PERIODS: dict[DocumentSource, int] = {
     # Polling is not supported so we fetch all doc permissions every 5 minutes
     DocumentSource.CONFLUENCE: 5 * 60,
-    DocumentSource.SLACK: 2 * 60,
+    DocumentSource.SLACK: 5 * 60,
 }
 
 
