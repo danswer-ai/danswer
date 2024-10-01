16ccc94e817424e221b7c7d2fe957df287a27ff3
pablodanswer <pablo@danswer.ai>
2024-10-01 14:55:01 -0700

    rm print

--- backend/danswer/server/query_and_chat/chat_backend.py
+++ backend/danswer/server/query_and_chat/chat_backend.py
@@ -332,7 +332,6 @@ def handle_new_chat_message(
                 ),
                 is_connected=is_disconnected_func,
             ):
-                print(packet)
                 yield json.dumps(packet) if isinstance(packet, dict) else packet
 
         except Exception as e:
