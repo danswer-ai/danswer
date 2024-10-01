b2a8bf76c345a7bb8b9268e1b3ab8a3b42439bed
Yuhong Sun <yuhongsun96@gmail.com>
2024-09-30 18:46:30 -0700

    k

--- backend/tests/daily/connectors/confluence/test_confluence_basic.py
+++ backend/tests/daily/connectors/confluence/test_confluence_basic.py
@@ -1,5 +1,7 @@
 import os
 import time
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
 import pytest
 
@@ -24,7 +26,12 @@ def confluence_connector() -> ConfluenceConnector:
     return connector
 
 
-def test_confluence_connector_basic(confluence_connector: ConfluenceConnector) -> None:
+@patch(
+    "danswer.file_processing.unstructured.get_unstructured_api_key", return_value=None
+)
+def test_confluence_connector_basic(
+    mock_get_api_key: MagicMock, confluence_connector: ConfluenceConnector
+) -> None:
     doc_batch_generator = confluence_connector.poll_source(0, time.time())
 
     doc_batch = next(doc_batch_generator)
