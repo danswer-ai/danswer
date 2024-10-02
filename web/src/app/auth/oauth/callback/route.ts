de30ef9c863f111d0b41853b1e117764d7213cdd
pablodanswer <pablo@danswer.ai>
2024-10-01 17:57:12 -0700

    nit

--- web/src/app/auth/oauth/callback/route.ts
+++ web/src/app/auth/oauth/callback/route.ts
@@ -16,7 +16,6 @@ export const GET = async (request: NextRequest) => {
       new URL("/auth/create-account", getDomain(request))
     );
   }
-  console.log(response);
 
   if (!setCookieHeader) {
     return NextResponse.redirect(new URL("/auth/error", getDomain(request)));
