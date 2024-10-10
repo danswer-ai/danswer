1581d354760723b97cb1786f91c4be68949709e6
pablodanswer <pablo@danswer.ai>
2024-10-10 12:34:30 -0700

    account for no visible assistants (#2765)

---
+++ web/src/components/modals/NoAssistantModal.tsx
@@ -0,0 +1,37 @@
+import { ModalWrapper } from "@/components/modals/ModalWrapper";
+
+export const NoAssistantModal = ({ isAdmin }: { isAdmin: boolean }) => {
+  return (
+    <ModalWrapper modalClassName="bg-white max-w-2xl rounded-lg shadow-xl text-center">
+      <>
+        <h2 className="text-3xl font-bold text-gray-800 mb-4">
+          No Assistant Available
+        </h2>
+        <p className="text-gray-600 mb-6">
+          You currently have no assistant configured. To use this feature, you
+          need to take action.
+        </p>
+        {isAdmin ? (
+          <>
+            <p className="text-gray-600 mb-6">
+              As an administrator, you can create a new assistant by visiting
+              the admin panel.
+            </p>
+            <button
+              onClick={() => {
+                window.location.href = "/admin/assistants";
+              }}
+              className="inline-flex flex justify-center items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-background-800 text-center focus:outline-none focus:ring-2 focus:ring-offset-2 "
+            >
+              Go to Admin Panel
+            </button>
+          </>
+        ) : (
+          <p className="text-gray-600 mb-2">
+            Please contact your administrator to configure an assistant for you.
+          </p>
+        )}
+      </>
+    </ModalWrapper>
+  );
+};
