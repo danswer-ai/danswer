1677efc7a4552adb9dfd0edba0ea209f7c7ffeb7
Weves <chrisweaver101@gmail.com>
2024-10-17 16:25:31 -0700

    Refactor + add global timeout env variable

--- backend/tests/daily/embedding/test_embeddings.py
+++ backend/tests/daily/embedding/test_embeddings.py
@@ -62,6 +62,26 @@ def test_cohere_embedding(cohere_embedding_model: EmbeddingModel) -> None:
 
 
 @pytest.fixture
+def litellm_embedding_model() -> EmbeddingModel:
+    return EmbeddingModel(
+        server_host="localhost",
+        server_port=9000,
+        model_name="text-embedding-3-small",
+        normalize=True,
+        query_prefix=None,
+        passage_prefix=None,
+        api_key="sk-7bHx9F2pL4mK3nQ8rT5vZ1yA6cE0wS",
+        provider_type=EmbeddingProvider.LITELLM,
+        api_url="http://ec2-3-21-37-122.us-east-2.compute.amazonaws.com:8000/embeddings",
+    )
+
+
+def test_litellm_embedding(litellm_embedding_model: EmbeddingModel) -> None:
+    _run_embeddings(VALID_SAMPLE, litellm_embedding_model, 1536)
+    _run_embeddings(TOO_LONG_SAMPLE, litellm_embedding_model, 1536)
+
+
+@pytest.fixture
 def local_nomic_embedding_model() -> EmbeddingModel:
     return EmbeddingModel(
         server_host="localhost",
