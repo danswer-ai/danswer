2e83f09def4220b41ebe1c6234b60a808e5ad090
Richard Kuo (Danswer) <rkuo@danswer.ai>
2024-10-22 14:10:55 -0700

    code review

--- backend/danswer/background/celery/configs/indexing.py
+++ backend/danswer/background/celery/configs/indexing.py
@@ -1,4 +1,5 @@
 import danswer.background.celery.configs.base as shared_config
+from danswer.configs.app_configs import CELERY_WORKER_INDEXING_CONCURRENCY
 
 broker_url = shared_config.broker_url
 broker_connection_retry_on_startup = shared_config.broker_connection_retry_on_startup
@@ -15,6 +16,6 @@ result_expires = shared_config.result_expires  # 86400 seconds is the default
 task_default_priority = shared_config.task_default_priority
 task_acks_late = shared_config.task_acks_late
 
-worker_concurrency = 1
+worker_concurrency = CELERY_WORKER_INDEXING_CONCURRENCY
 worker_pool = "threads"
 worker_prefetch_multiplier = 1
