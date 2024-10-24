5cfe08de8b1d595f68fbfc3d25b09e4039f69ce5
pablodanswer <pablo@danswer.ai>
2024-10-24 14:01:33 -0700

    nit

--- backend/danswer/db/persona.py
+++ backend/danswer/db/persona.py
@@ -328,6 +328,7 @@ def update_all_personas_display_priority(
 
     for persona in personas:
         persona.display_priority = display_priority_map[persona.id]
+    db_session.commit()
 
 
 def upsert_prompt(
