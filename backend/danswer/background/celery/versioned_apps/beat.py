01bdcad4f038c5d4c642ca14680593988c28bf96
pablodanswer <pablo@danswer.ai>
2024-11-02 12:57:56 -0700

    ensure versioned apps capture

--- backend/danswer/background/celery/versioned_apps/beat.py
+++ backend/danswer/background/celery/versioned_apps/beat.py
@@ -1,8 +1,6 @@
 """Factory stub for running celery worker / celery beat."""
-from danswer.utils.variable_functionality import fetch_versioned_implementation
+from danswer.background.celery.apps.beat import celery_app
 from danswer.utils.variable_functionality import set_is_ee_based_on_env_variable
 
 set_is_ee_based_on_env_variable()
-app = fetch_versioned_implementation(
-    "danswer.background.celery.apps.beat", "celery_app"
-)
+app = celery_app
