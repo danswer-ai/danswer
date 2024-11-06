f457bdb49128b010da04612f598ef0e0810dcf7c
pablodanswer <pablo@danswer.ai>
2024-11-06 14:11:41 -0800

    update starter message

--- web/src/components/assistants/StarterMessage.tsx
+++ web/src/components/assistants/StarterMessage.tsx
@@ -1,7 +1,6 @@
-import {
-  Persona,
-  StarterMessage as StarterMessageType,
-} from "../../app/admin/assistants/interfaces";
+import { useContext } from "react";
+import { Persona } from "../../app/admin/assistants/interfaces";
+import { SettingsContext } from "../settings/SettingsProvider";
 
 export function StarterMessages({
   currentPersona,
@@ -10,32 +9,36 @@ export function StarterMessages({
   currentPersona: Persona;
   onSubmit: (messageOverride: string) => void;
 }) {
+  const settings = useContext(SettingsContext);
+  const isMobile = settings?.isMobile;
   return (
     <div
       key={-4}
       className={`
         mx-auto
-        px-4
         w-full
-        max-w-[750px]
+        ${
+          isMobile
+            ? "gap-x-2 w-2/3 justify-between"
+            : "justify-center max-w-[750px] items-start"
+        }
         flex
-        flex-wrap
-        justify-center
         mt-6
-        items-start
       `}
     >
       {currentPersona?.starter_messages &&
         currentPersona.starter_messages.length > 0 && (
           <>
             {currentPersona.starter_messages
-              .slice(0, 4)
+              .slice(0, isMobile ? 2 : 4)
               .map((starterMessage, i) => (
-                <div key={i} className="w-1/4">
+                <div key={i} className={`${isMobile ? "w-1/2" : "w-1/4"}`}>
                   <button
                     onClick={() => onSubmit(starterMessage.message)}
-                    className={`relative flex w-40 flex-col gap-2 rounded-2xl shadow-sm border border-border px-3 py-2 text-start align-top text-[15px] shadow-xxs transition enabled:hover:bg-background-100 disabled:cursor-not-allowed line-clamp-3`}
-                    style={{ height: `5.4rem` }}
+                    className={`relative flex ${
+                      !isMobile && "w-40"
+                    } flex-col gap-2 rounded-2xl shadow-sm border border-border px-3 py-2 text-start align-to text-wrap text-[15px] shadow-xs transition enabled:hover:bg-background-100 disabled:cursor-not-allowed line-clamp-3`}
+                    style={{ height: `5.2rem` }}
                   >
                     {starterMessage.name}
                   </button>
