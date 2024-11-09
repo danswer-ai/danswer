2d9586b059cfb1cb8e1f6c0fccc696af6ba8873d
pablodanswer <pablo@danswer.ai>
2024-11-08 19:15:29 -0800

    nit

--- web/src/app/admin/connector/[ccPairId]/page.tsx
+++ web/src/app/admin/connector/[ccPairId]/page.tsx
@@ -283,7 +283,7 @@ export default function Page(props: { params: Promise<{ ccPairId: string }> }) {
   const ccPairId = parseInt(params.ccPairId);
 
   return (
-    <div className="container">
+    <div className="mx-auto container">
       <Main ccPairId={ccPairId} />
     </div>
   );
