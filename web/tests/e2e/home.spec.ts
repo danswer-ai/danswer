4743909f838172bfd8d5875be365256f67ab9055
Richard Kuo (Danswer) <rkuo@danswer.ai>
2024-11-13 14:22:04 -0800

    preliminary working config

--- web/tests/e2e/home.spec.ts
+++ web/tests/e2e/home.spec.ts
@@ -3,10 +3,10 @@ import { test, expect } from "@chromatic-com/playwright";
 
 // Then use as normal 👇
 test("Homepage", async ({ page }) => {
-  await page.goto("http://localhost:3000");
+  await page.goto("http://localhost:3000/search");
 
   await expect(page).toHaveTitle("Danswer");
 
   // Take a screenshot of the basic home page
-  page.screenshot({ path: "home.png" });
+  // page.screenshot({ path: "home.png" });
 });
