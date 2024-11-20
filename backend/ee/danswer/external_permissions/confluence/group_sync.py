67d943da119a33515909307e1d49634ef88c4abf
hagen-danswer <hagen@danswer.ai>
2024-11-20 10:40:21 -0800

    added logging and bugfixing to conf (#3167)
    * standardized escaping of CQL strings
    
    * think i found it
    
    * fix
    
    * should be fixed
    
    * added handling for special linking behavior in confluence
    
    * Update onyx_confluence.py
    
    * Update onyx_confluence.py
    
    ---------
    
    Co-authored-by: rkuo-danswer <rkuo@danswer.ai>

--- backend/ee/danswer/external_permissions/confluence/group_sync.py
+++ backend/ee/danswer/external_permissions/confluence/group_sync.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from sqlalchemy.orm import Session
 
 from danswer.connectors.confluence.onyx_confluence import OnyxConfluence
@@ -19,12 +17,8 @@ def _get_group_members_email_paginated(
     confluence_client: OnyxConfluence,
     group_name: str,
 ) -> set[str]:
-    members: list[dict[str, Any]] = []
-    for member_batch in confluence_client.paginated_group_members_retrieval(group_name):
-        members.extend(member_batch)
-
     group_member_emails: set[str] = set()
-    for member in members:
+    for member in confluence_client.paginated_group_members_retrieval(group_name):
         email = member.get("email")
         if not email:
             user_name = member.get("username")
@@ -52,10 +46,9 @@ def confluence_group_sync(
 
     # Get all group names
     group_names: list[str] = []
-    for group_batch in confluence_client.paginated_groups_retrieval():
-        for group in group_batch:
-            if group_name := group.get("name"):
-                group_names.append(group_name)
+    for group in confluence_client.paginated_groups_retrieval():
+        if group_name := group.get("name"):
+            group_names.append(group_name)
 
     # For each group name, get all members and create a danswer group
     danswer_groups: list[ExternalUserGroup] = []
