fa592a1b7a69897110a928a222b19eaef3b7267a
pablodanswer <pablo@danswer.ai>
2024-11-21 10:52:08 -0800

    clean horizontal scrollbar

--- web/src/components/search/SearchSection.tsx
+++ web/src/components/search/SearchSection.tsx
@@ -698,7 +698,7 @@ export const SearchSection = ({
           </div>
         </div>
 
-        <div className="absolute include-scrollbar h-screen overflow-y-auto left-0 w-full top-0">
+        <div className="absolute include-horizontal-scrollbar h-screen overflow-y-auto left-0 w-full top-0">
           <FunctionalHeader
             sidebarToggled={toggledSidebar}
             reset={() => setQuery("")}
