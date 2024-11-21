1744d29bd6f6740fb20bbbf8b5651cd60edbf127
pablodanswer <pablo@danswer.ai>
2024-11-21 11:15:05 -0800

    k

--- web/tailwind-themes/tailwind.config.js
+++ web/tailwind-themes/tailwind.config.js
@@ -83,7 +83,7 @@ module.exports = {
         "non-selectable": "var(--non-selectable)",
 
         // background
-        background: "var(--background)",
+        background: "#EDEDF1",
         "background-100": "var(--background-100)",
         "background-125": "var(--background-125)",
         "background-150": "var(--background-150)",
@@ -95,53 +95,40 @@ module.exports = {
         "background-700": "var(--background-700)",
         "background-800": "var(--background-800)",
         "background-900": "var(--background-900)",
+        "background-toggle": "var(--background-100)",
 
+        "toggled-background": "var(--background-500)",
+        "untoggled-background": "var(--background-300)",
         "background-inverted": "var(--background-inverted)",
         "background-emphasis": "var(--background-emphasis)",
         "background-strong": "var(--background-strong)",
         "background-search": "var(--white)",
 
-        "background-history-sidebar-button-hover": "var(--background-200)",
-        "text-history-sidebar-button": "var(--text-800)",
-        "divider-history-sidebar-bar": "var(--border)",
-        "text-mobile-sidebar": "var(--text-800)",
-        "background-search-filter": "var(--background-100)",
-        "background-search-filter-dropdown": "var(--background-100)",
+        "text-sidebar-toggled-header": "var(--text-200)",
+        "text-sidebar-header": "var(--text-900)",
+        "text-sidebar-dark": "var(--text-200)",
 
+        "search-answer-border": "var(--background-300)",
+
+        "background-chat-hover": "#1D4ED8",
+        "background-chat-selected": "#1D4ED8",
         // colors for sidebar in chat, search, and manage settings
-        "background-sidebar": "var(--background-100)",
-        "background-chatbar": "var(--background-100)",
-        "text-sidebar": "var(--text-500)",
-
-        "toggled-background": "var(--background-400)",
-        "untoggled-background": "var(--background-200)",
-        "background-starter-message": "var(--background)",
-        "background-starter-message-hover": "var(--background-100)",
-
-        "text-sidebar-toggled-header": "var(--text-800)",
-        "text-sidebar-header": "var(--text-800)",
-
-        "background-back-button": "var(--background-200)",
-        "text-back-button": "var(--text-800)",
+        "background-sidebar": "#0021A5",
+        "background-settings-sidebar": "var(--background-100)",
+        "background-chatbar": "#FFFFFF",
+        "text-sidebar": "#FFFFFF",
 
         // Settings
-        "text-sidebar-subtle": "var(--text-500)",
-        "icon-settings-sidebar": "var(--text-600)",
-        "text-settings-sidebar": "var(--text-600)",
-        "text-settings-sidebar-strong": "var(--text-900)",
-        "background-settings-hover": "var(--background-200)",
-
-        "text-application-toggled": "var(--text-800)",
-        "text-application-untoggled": "var(--text-500)",
-        "text-application-untoggled-hover": "var(--text-700)",
-
-        "background-chat-hover": "var(--background-200)",
-        "background-chat-selected": "var(--background-200)",
+        "text-sidebar-subtle": "#A9D4FF",
+        "icon-settings-sidebar": "var(--text-300)",
+        "text-settings-sidebar": "var(--text-300)",
+        "text-settings-sidebar-strong": "var(--text-200)",
+        "background-settings-hover": "#0026CC",
+        "background-back-button": "#A9D4FF",
+        "text-back-button": "var(--text-800)",
 
         // Background for chat messages (user bubbles)
-        user: "var(--user-bubble)",
-
-        "background-toggle": "var(--background-100)",
+        user: "#0021A5",
 
         // Colors for the search toggle buttons
         "background-agentic-toggled": "var(--light-success)",
@@ -155,8 +142,10 @@ module.exports = {
         "star-indicator": "var(--background-100)",
 
         // Backgrounds for submit buttons on search and chat
-        "submit-background": "var(--background-800)",
+        "submit-background": "#0021A5",
         "disabled-submit-background": "var(--background-400)",
+
+        "sidebar-toggle": "var(--text-200)",
 
         input: "var(--white)",
 
@@ -172,7 +161,7 @@ module.exports = {
         "text-900": "var(--text-900)",
         "text-950": "var(--text-950)",
 
-        "user-text": "var(--text-800)",
+        "user-text": "#FFFFFF",
 
         description: "var(--text-400)",
         subtle: "var(--text-500)",
