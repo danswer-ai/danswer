e3573b2bc1e345f1b9e4d3185289e83cc80d9db7
pablodanswer <pablo@danswer.ai>
2024-11-21 17:11:11 -0800

    add comment

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -36,6 +36,7 @@ export const SIDEBAR_WIDTH = `w-[350px]`;
 export const LOGOUT_DISABLED =
   process.env.NEXT_PUBLIC_DISABLE_LOGOUT?.toLowerCase() === "true";
 
+// Default sidebar open is true if the environment variable is not set
 export const NEXT_PUBLIC_DEFAULT_SIDEBAR_OPEN =
   process.env.NEXT_PUBLIC_DEFAULT_SIDEBAR_OPEN?.toLowerCase() === "true" ??
   true;
