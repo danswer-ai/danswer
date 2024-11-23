7afb27a4600aa6f717c1ad6ffab2069faf1525bb
Richard Kuo (Danswer) <rkuo@danswer.ai>
2024-11-22 17:30:29 -0800

    name variable correctly

--- backend/danswer/connectors/confluence/onyx_confluence.py
+++ backend/danswer/connectors/confluence/onyx_confluence.py
@@ -294,7 +294,7 @@ def _validate_connector_configuration(
     wiki_base: str,
 ) -> None:
     # test connection with direct client, no retries
-    confluence_client_without_retries = Confluence(
+    confluence_client_with_minimal_retries = Confluence(
         api_version="cloud" if is_cloud else "latest",
         url=wiki_base.rstrip("/"),
         username=credentials["confluence_username"] if is_cloud else None,
@@ -304,7 +304,7 @@ def _validate_connector_configuration(
         max_backoff_retries=6,
         max_backoff_seconds=10,
     )
-    spaces = confluence_client_without_retries.get_all_spaces(limit=1)
+    spaces = confluence_client_with_minimal_retries.get_all_spaces(limit=1)
 
     if not spaces:
         raise RuntimeError(
