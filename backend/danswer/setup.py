ca418fdcf28b267708fbeeef1b96887d6b2f1bc9
pablodanswer <pablo@danswer.ai>
2024-11-27 12:16:42 -0800

    order seeding

--- backend/danswer/setup.py
+++ backend/danswer/setup.py
@@ -254,13 +254,14 @@ def setup_postgres(db_session: Session) -> None:
     create_initial_public_credential(db_session)
     create_initial_default_connector(db_session)
     associate_default_cc_pair(db_session)
-
-    logger.notice("Loading default Prompts and Personas")
     delete_old_default_personas(db_session)
-    load_chat_yamls(db_session)
 
     logger.notice("Loading built-in tools")
     load_builtin_tools(db_session)
+
+    logger.notice("Loading default Prompts and Personas")
+    load_chat_yamls(db_session)
+
     refresh_built_in_tools_cache(db_session)
     auto_add_search_tool_to_personas(db_session)
 
