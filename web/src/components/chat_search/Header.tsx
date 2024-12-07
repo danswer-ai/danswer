c3cb97192a58826efce0d41a18ef088e8a39d527
pablodanswer <pablo@danswer.ai>
2024-12-07 15:40:39 -0800

    minor

--- web/src/components/chat_search/Header.tsx
+++ web/src/components/chat_search/Header.tsx
@@ -133,7 +133,7 @@ export default function FunctionalHeader({
             }
           >
             <div className="cursor-pointer mr-4 flex-none text-text-700 hover:text-text-600 transition-colors duration-300">
-              {/* <NewChatIcon size={20} /> */}
+              <NewChatIcon size={20} />
             </div>
           </Link>
         </div>
