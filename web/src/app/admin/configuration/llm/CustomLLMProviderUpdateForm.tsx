75a448c8b2be1362e6aca6de7317611ce4859c83
Weves <chrisweaver101@gmail.com>
2024-12-09 13:13:25 -0800

    Add better vertex support + LLM form cleanup

--- web/src/app/admin/configuration/llm/CustomLLMProviderUpdateForm.tsx
+++ web/src/app/admin/configuration/llm/CustomLLMProviderUpdateForm.tsx
@@ -275,8 +275,9 @@ export function CustomLLMProviderUpdateForm({
             <SubLabel>
               <>
                 <div>
-                  Additional configurations needed by the model provider. Are
-                  passed to litellm via environment variables.
+                  Additional configurations needed by the model provider. These
+                  are passed to litellm via environment + as arguments into the
+                  `completion` call.
                 </div>
 
                 <div className="mt-2">
@@ -290,14 +291,14 @@ export function CustomLLMProviderUpdateForm({
             <FieldArray
               name="custom_config_list"
               render={(arrayHelpers: ArrayHelpers<any[]>) => (
-                <div>
+                <div className="w-full">
                   {formikProps.values.custom_config_list.map((_, index) => {
                     return (
                       <div
                         key={index}
-                        className={index === 0 ? "mt-2" : "mt-6"}
+                        className={(index === 0 ? "mt-2" : "mt-6") + " w-full"}
                       >
-                        <div className="flex">
+                        <div className="flex w-full">
                           <div className="w-full mr-6 border border-border p-3 rounded">
                             <div>
                               <Label>Key</Label>
@@ -457,6 +458,7 @@ export function CustomLLMProviderUpdateForm({
                   <Button
                     type="button"
                     variant="destructive"
+                    className="ml-3"
                     icon={FiTrash}
                     onClick={async () => {
                       const response = await fetch(
