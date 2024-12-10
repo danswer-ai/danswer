878a189011a8e7cef94d705c8b4a3dd1eec64fcc
pablonyx <pablo@danswer.ai>
2024-12-10 13:36:40 -0800

    delete input prompts (#3380)
    * delete input prompts
    
    * nit
    
    * remove vestigial test
    
    * nit

--- web/src/app/chat/sessionSidebar/HistorySidebar.tsx
+++ web/src/app/chat/sessionSidebar/HistorySidebar.tsx
@@ -163,15 +163,6 @@ export const HistorySidebar = forwardRef<HTMLDivElement, HistorySidebarProps>(
                   Manage Assistants
                 </p>
               </Link>
-              <Link
-                href="/prompts"
-                className="w-full p-2 bg-white border-border border rounded items-center hover:bg-background-history-sidebar-button-hover cursor-pointer transition-all duration-150 flex gap-x-2"
-              >
-                <ClosedBookIcon className="h-4 w-4 my-auto text-text-history-sidebar-button" />
-                <p className="my-auto flex items-center text-sm ">
-                  Manage Prompts
-                </p>
-              </Link>
             </div>
           )}
           <div className="border-b border-divider-history-sidebar-bar pb-4 mx-3" />
