739edc87ccaedacee2598a70542f0adc53e51cd8
Richard Kuo (Danswer) <rkuo@onyx.app>
2024-12-11 10:59:39 -0800

    cleaned imports and commented code

--- web/src/app/admin/background/BackgroundIndexingStatusTable.tsx
+++ web/src/app/admin/background/BackgroundIndexingStatusTable.tsx
@@ -1,47 +1,16 @@
-import React, { useState, useMemo, useEffect, useRef } from "react";
+import { SourceIcon } from "@/components/SourceIcon";
 import {
   Table,
-  TableRow,
-  TableHead,
   TableBody,
   TableCell,
+  TableHead,
   TableHeader,
+  TableRow,
 } from "@/components/ui/table";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { IndexAttemptStatus } from "@/components/Status";
-import { timeAgo } from "@/lib/time";
-import {
-  ConnectorBackgroundIndexingStatus,
-  ConnectorIndexingStatus,
-  ConnectorSummary,
-  GroupedConnectorSummaries,
-  ValidSources,
-} from "@/lib/types";
-import { useRouter } from "next/navigation";
-import {
-  FiChevronDown,
-  FiChevronRight,
-  FiSettings,
-  FiLock,
-  FiUnlock,
-  FiRefreshCw,
-  FiPauseCircle,
-} from "react-icons/fi";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { SourceIcon } from "@/components/SourceIcon";
-import { getSourceDisplayName } from "@/lib/sources";
-import { CustomTooltip } from "@/components/tooltip/CustomTooltip";
-import { Warning } from "@phosphor-icons/react";
-import Cookies from "js-cookie";
-import { TOGGLED_CONNECTORS_COOKIE_NAME } from "@/lib/constants";
-import { usePaidEnterpriseFeaturesEnabled } from "@/components/settings/usePaidEnterpriseFeaturesEnabled";
 import { getTimeElapsedString } from "@/lib/dateUtils";
+import { getSourceDisplayName } from "@/lib/sources";
+import { ConnectorBackgroundIndexingStatus, ValidSources } from "@/lib/types";
+import React from "react";
 
 function SummaryRow({ status }: { status: ConnectorBackgroundIndexingStatus }) {
   let elapsedString = "Waiting to start...";
@@ -85,44 +54,6 @@ function SummaryRow({ status }: { status: ConnectorBackgroundIndexingStatus }) {
     </TableRow>
   );
 }
-
-// export function BackgroundIndexingStatusTable({
-//   indexingStatuses,
-// }: {
-//   indexingStatuses: ConnectorBackgroundIndexingStatus[];
-// }) {
-
-//   return (
-//     <Table>
-//       <TableHeader>
-//       </TableHeader>
-//       <TableBody>
-//       {/* <TableRow className="border border-border">
-//                         <TableHead>Name</TableHead>
-//                         <TableHead>CC Pair</TableHead>
-//                         <TableHead>Search Settings</TableHead>
-//                         <TableHead>Progress</TableHead>
-//                         <TableHead>Elapsed</TableHead>
-//                       </TableRow> */}
-//       indexingStatuses.map((status, index) => ()
-//               return (
-//                 <React.Fragment key={ind}>
-//                   <br className="mt-4" />
-
-//                       {indexingStatuses.map((status) => (
-//                         <SummaryRow
-//                     source={source}
-//                     summary={groupSummaries[source]}
-//                     isOpen={connectorsToggled[source] || false}
-//                     onToggle={() => toggleSource(source)}
-//                   />
-//                   </React.Fragment>
-//                       )
-//       }
-//       </TableBody>
-//     </Table>
-//   );
-// }
 
 export function BackgroundIndexingStatusTable({
   indexingStatuses,
