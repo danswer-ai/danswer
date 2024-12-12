d0534b20082814bdd195599d480e76f65579004b
Richard Kuo <rkuo@rkuo.com>
2024-12-11 22:14:08 -0800

    fix a test

--- web/tests/e2e/admin_users.spec.ts
+++ web/tests/e2e/admin_users.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@chromatic-com/playwright";
 
 test(
-  "Admin - User Management - Groups",
+  "Admin - User Management - Users",
   {
     tag: "@admin",
   },
@@ -9,11 +9,6 @@ test(
     // Test simple loading
     await page.goto("http://localhost:3000/admin/users");
     await expect(page.locator("h1.text-3xl")).toHaveText("Manage Users");
-    await expect(page.locator("div.font-bold").nth(0)).toHaveText(
-      "Invited Users"
-    );
-    await expect(page.locator("div.font-bold").nth(1)).toHaveText(
-      "Current Users"
-    );
+    await expect(page.locator("h3.text-2xl")).toHaveText("Invited Users");
   }
 );
