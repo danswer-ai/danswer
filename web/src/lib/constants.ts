4c131ff579163795b0bc40da3b68b32cc2369380
Richard Kuo (Danswer) <rkuo@onyx.app>
2024-12-12 11:00:20 -0800

    add support for NEXT_PUBLIC_TEST_ENV

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -38,8 +38,9 @@ export const LOGOUT_DISABLED =
 
 // Default sidebar open is true if the environment variable is not set
 export const NEXT_PUBLIC_DEFAULT_SIDEBAR_OPEN =
-  process.env.NEXT_PUBLIC_DEFAULT_SIDEBAR_OPEN?.toLowerCase() === "true" ??
-  true;
+  process.env.NEXT_PUBLIC_DEFAULT_SIDEBAR_OPEN === undefined ||
+  process.env.NEXT_PUBLIC_DEFAULT_SIDEBAR_OPEN === "" ||
+  process.env.NEXT_PUBLIC_DEFAULT_SIDEBAR_OPEN?.toLowerCase() === "true";
 
 export const TOGGLED_CONNECTORS_COOKIE_NAME = "toggled_connectors";
 
@@ -77,4 +78,5 @@ export const REGISTRATION_URL =
 export const SERVER_SIDE_ONLY__CLOUD_ENABLED =
   process.env.NEXT_PUBLIC_CLOUD_ENABLED?.toLowerCase() === "true";
 
-export const TEST_ENV = process.env.TEST_ENV?.toLowerCase() === "true";
+export const NEXT_PUBLIC_TEST_ENV =
+  process.env.NEXT_PUBLIC_TEST_ENV?.toLowerCase() === "true";
