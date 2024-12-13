3bb9e4bff6069bec5c7a2bc8b563df0ad5510c29
pablodanswer <pablo@danswer.ai>
2024-12-13 10:06:07 -0800

    post rebase fix

--- backend/alembic/versions/54a74a0417fc_danswerbot_onyxbot.py
+++ backend/alembic/versions/54a74a0417fc_danswerbot_onyxbot.py
@@ -1,7 +1,7 @@
 """danswerbot -> onyxbot
 
 Revision ID: 54a74a0417fc
-Revises: bf7a81109301
+Revises: 94dc3d0236f8
 Create Date: 2024-12-11 18:05:05.490737
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "54a74a0417fc"
-down_revision = "bf7a81109301"
+down_revision = "94dc3d0236f8"
 branch_labels = None
 depends_on = None
 
