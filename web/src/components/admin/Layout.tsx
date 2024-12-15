2bc0084116aac6a78e57c15571d03d91a8d1b5de
pablodanswer <pablo@danswer.ai>
2024-12-14 16:34:47 -0800

    cloud settings -> billing

--- web/src/components/admin/Layout.tsx
+++ web/src/components/admin/Layout.tsx
@@ -7,7 +7,7 @@ import {
 import { redirect } from "next/navigation";
 import { ClientLayout } from "./ClientLayout";
 import {
-  SERVER_SIDE_ONLY__CLOUD_ENABLED,
+  NEXT_PUBLIC_CLOUD_ENABLED,
   SERVER_SIDE_ONLY__PAID_ENTERPRISE_FEATURES_ENABLED,
 } from "@/lib/constants";
 import { AnnouncementBanner } from "../header/AnnouncementBanner";
@@ -45,7 +45,7 @@ export async function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ClientLayout
       enableEnterprise={SERVER_SIDE_ONLY__PAID_ENTERPRISE_FEATURES_ENABLED}
-      enableCloud={SERVER_SIDE_ONLY__CLOUD_ENABLED}
+      enableCloud={NEXT_PUBLIC_CLOUD_ENABLED}
       user={user}
     >
       <AnnouncementBanner />
