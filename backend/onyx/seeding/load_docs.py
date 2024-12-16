d0483dd269109df084c5ea27c74b07886879be28
pablodanswer <pablo@danswer.ai>
2024-12-15 21:41:21 -0800

    temporary vespa bump for tests

--- backend/onyx/seeding/load_docs.py
+++ backend/onyx/seeding/load_docs.py
@@ -217,7 +217,7 @@ def seed_initial_documents(
     # as we just sent over the Vespa schema and there is a slight delay
 
     index_with_retries = retry_builder()(document_index.index)
-    index_with_retries(chunks=chunks, fresh_index=True)
+    index_with_retries(chunks=chunks, fresh_index=cohere_enabled)
 
     # Mock a run for the UI even though it did not actually call out to anything
     mock_successful_index_attempt(
