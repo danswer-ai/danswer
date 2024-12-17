f1cac05fdd043e4e532cd31c8c4221a18ea817a5
pablodanswer <pablo@danswer.ai>
2024-12-17 15:57:31 -0800

    add log line

--- backend/ee/onyx/utils/telemetry.py
+++ backend/ee/onyx/utils/telemetry.py
@@ -2,6 +2,9 @@ from posthog import Posthog
 
 from ee.onyx.configs.app_configs import POSTHOG_API_KEY
 from ee.onyx.configs.app_configs import POSTHOG_HOST
+from onyx.utils.logger import setup_logger
+
+logger = setup_logger()
 
 posthog = Posthog(project_api_key=POSTHOG_API_KEY, host=POSTHOG_HOST)
 
@@ -11,4 +14,5 @@ def event_telemetry(
     event: str,
     properties: dict | None = None,
 ) -> None:
+    logger.info(f"Capturing Posthog event: {distinct_id} {event} {properties}")
     posthog.capture(distinct_id, event, properties)
